7579ad39a790405f1b79742593a5b9900b701279
akhilaraghavan <akhila.raghavan@zysk.tech>
2023-08-21 11:21:25 +0530

    UPDATE:work in progress

--- app/Livewire/VendorRegistration.php
+++ app/Livewire/VendorRegistration.php
@@ -3,11 +3,17 @@
 namespace App\Livewire;
 
 use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
 use Livewire\Component;
-use Filament\Forms\Components\Contracts\HasForms;
-class VendorRegistration extends Component 
+class VendorRegistration extends Component implements HasForms
 {
     use InteractsWithForms;
+    public ?array $data = [];
+
+    public function mount(): void
+    {
+        $this->form->fill();
+    }
     public function render()
     {
         return view('livewire.vendor-registration');
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Livewire\VendorRegistration;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,4 @@ Route::middleware(['auth:sanctum', 'verified'])
 Route::prefix('/')
     ->middleware(['auth:sanctum', 'verified'])
     ->group(function () {});
+Route::get('Vendor/create', VendorRegistration::class);
