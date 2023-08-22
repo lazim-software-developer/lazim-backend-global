90e7b27e20a234d3dac76a0a5e995149cd38cf18
akhilaraghavan <akhila.raghavan@zysk.tech>
2023-08-22 18:32:47 +0530

    Update:work in progress

--- app/Filament/Resources/SnaggingResource/Pages/CreateSnagging.php
+++ app/Filament/Resources/SnaggingResource/Pages/CreateSnagging.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\SnaggingResource\Pages;
 
 use App\Filament\Resources\SnaggingResource;
 use App\Models\Building\Complaint;
+use App\Models\User\User;
 use Doctrine\DBAL\Types\JsonType;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
@@ -11,15 +12,14 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateSnagging extends CreateRecord
 {
     protected static string $resource = SnaggingResource::class;
-    // protected function afterCreate(){
-    //     $jsonValue =JsonType([]);
-    //     Complaint::where('id',$this->record->id)
-    //             ->update([
-    //                 "remarks"=>[{
-    //                     "comment":$this->record->remark}
-    //                     ,"date":now();
-    //                     ,"user":user->name;}]
+    protected function afterCreate(){
+        $jsonValue = json_encode(['comment' => $this->record->remarks,'date'=>now(),
+        'user'=> User::where('id',$this->record->user_id)->first()->first_name
+    ]);
 
-    //             ]);
-    // }
+        Complaint::where('id', $this->record->id)
+            ->update([
+                'remarks' => $jsonValue
+            ]);
+    }
 }
