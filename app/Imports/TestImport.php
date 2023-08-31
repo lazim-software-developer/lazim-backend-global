f1a4d35167d1599a8edff16a55ecf0be97dd2530
Manjula-GK <manjula.govindappa@zysk.tech>
2023-08-31 08:52:46 -0700

    import file

--- app/Imports/TestImport.php
+++ app/Imports/TestImport.php
@@ -5,7 +5,9 @@ namespace App\Imports;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class TestImport implements ToModel, WithHeadingRow
+class
+
+TestImport implements ToModel, WithHeadingRow
 {
     public function model(array $row)
     {
