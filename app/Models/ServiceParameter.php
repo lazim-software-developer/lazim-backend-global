4720b57145288f7b79bfc154f5facbb9703fc1d7
Susheel-zysk <susheel.k@zysk.tech>
2023-09-01 19:00:19 +0530

    added models and migartion files

---
+++ app/Models/ServiceParameter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ServiceParameter extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'active'
+    ];
+
+    protected $searchableFields = ['*'];
+
+    protected $table = 'service_parameters';
+
+    protected $casts = [
+        'active' => 'boolean'
+    ];
+
+}
