28034bceb7828d789d590848fce3716ce407aa30
Suthesh-M <suthesh.m@zysk.tech>
2023-09-06 11:10:18 +0530

    OaServiceRequestResource updated

--- app/Http/Resources/OaServiceRequestResource.php
+++ app/Http/Resources/OaServiceRequestResource.php
@@ -15,6 +15,7 @@ class OaServiceRequestResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id'                => $this->id,
             'service_parameter' => $this->serviceParameter->name,
             'property_group'    => $this->property_group,
             'from_date'         => $this->from_date,
