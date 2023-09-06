a234e83feeb6564b1e2d7715867b1528ed846e4f
Suthesh-M <suthesh.m@zysk.tech>
2023-09-06 11:08:37 +0530

    OaServiceRequestResource updated

--- app/Http/Resources/OaServiceRequestResource.php
+++ app/Http/Resources/OaServiceRequestResource.php
@@ -25,18 +25,21 @@ class OaServiceRequestResource extends JsonResource
             'created_date'      => $this->created_at,
             'oa_service_file'   => isset($this->oa_service_file) ?
             [
-                'e_services'             => env('AWS_URL') . '/' . $this->oa_service_file . '/e_services.xlsx',
-                'happiness_center'       => env('AWS_URL') . '/' . $this->oa_service_file . '/happiness_center.xlsx',
-                'balance_sheet'          => env('AWS_URL') . '/' . $this->oa_service_file . '/balance_sheet.xlsx',
-                'reserve_fund'           => env('AWS_URL') . '/' . $this->oa_service_file . '/reserve_fund.xlsx',
-                'budget_vs_actual'       => env('AWS_URL') . '/' . $this->oa_service_file . '/budget_vs_actual.xlsx',
-                'accounts_payables'      => env('AWS_URL') . '/' . $this->oa_service_file . '/accounts_payables.xlsx',
-                'delinquents'            => env('AWS_URL') . '/' . $this->oa_service_file . '/delinquents.xlsx',
-                'collections'            => env('AWS_URL') . '/' . $this->oa_service_file . '/collections.xlsx',
-                'work_orders'            => env('AWS_URL') . '/' . $this->oa_service_file . '/work_orders.xlsx',
-                'central_fund_statement' => env('AWS_URL') . '/' . $this->oa_service_file .
+                'e_services'              => env('AWS_URL') . '/' . $this->oa_service_file . '/e_services.xlsx',
+                'happiness_center'        => env('AWS_URL') . '/' . $this->oa_service_file . '/happiness_center.xlsx',
+                'balance_sheet'           => env('AWS_URL') . '/' . $this->oa_service_file . '/balance_sheet.xlsx',
+                'reserve_fund'            => env('AWS_URL') . '/' . $this->oa_service_file . '/reserve_fund.xlsx',
+                'budget_vs_actual'        => env('AWS_URL') . '/' . $this->oa_service_file . '/budget_vs_actual.xlsx',
+                'accounts_payables'       => env('AWS_URL') . '/' . $this->oa_service_file . '/accounts_payables.xlsx',
+                'delinquents'             => env('AWS_URL') . '/' . $this->oa_service_file . '/delinquents.xlsx',
+                'collections'             => env('AWS_URL') . '/' . $this->oa_service_file . '/collections.xlsx',
+                'work_orders'             => env('AWS_URL') . '/' . $this->oa_service_file . '/work_orders.xlsx',
+                'central_fund_statement'  => env('AWS_URL') . '/' . $this->oa_service_file .
                 '/central_fund_statement.xlsx',
-                'bank_balance'           => env('AWS_URL') . '/' . $this->oa_service_file . '/bank_balance.xlsx',
+                'bank_balance'            => env('AWS_URL') . '/' . $this->oa_service_file . '/bank_balance.xlsx',
+                'asset_list_and_expenses' => env('AWS_URL') . '/' . $this->oa_service_file .
+                '/asset_list_and_expenses.xlsx',
+                'utility_expenses'        => env('AWS_URL') . '/' . $this->oa_service_file . '/utility_expenses.xlsx',
             ] : null,
         ];
     }
