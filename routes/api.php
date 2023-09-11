750ea9bf7d9b5647c0eb23ae82c4ff7fa3d8e2e2
Suthesh-M <suthesh.m@zysk.tech>
2023-09-11 10:48:38 +0530

    auth middleware added to all APIs

--- routes/api.php
+++ routes/api.php
@@ -27,31 +27,32 @@ Route::middleware('auth:sanctum')
 Route::name('api.')
     ->middleware('auth:sanctum')
     ->group(function () {
+        Route::post('upload/1', [TestController::class, 'uploadEservices']);
+
+        Route::post('upload/2', [TestController::class, 'uploadhappinessCenter']);
+
+        Route::post('upload/3', [TestController::class, 'uploadBalanceSheet']);
+
+        Route::post('upload/4', [TestController::class, 'uploadGeneralFund']);
+
+        Route::post('upload/5', [TestController::class, 'uploadReservedFund']);
+
+        Route::post('upload/6', [TestController::class, 'uploadBudgetVsActual']);
+
+        Route::post('upload/7', [TestController::class, 'uploadAccountsPayable']);
+
+        Route::post('upload/8', [TestController::class, 'uploadDelinquents']);
+
+        Route::post('upload/9', [TestController::class, 'uploadCollection']);
+
+        Route::post('upload/12', [TestController::class, 'uploadWorkOrders']);
+
+        Route::get('services-requests', [TestController::class, 'serviceRequest']);
+
+        Route::get('service-parameters', [TestController::class, 'serviceParameters']);
+
+        Route::post('upload-all', [TestController::class, 'uploadAll']);
+
+        Route::get('oa-service-details/{oaService}', [TestController::class, 'getOaService']);
+
     });
-    Route::post('upload/1', [TestController::class, 'uploadEservices']);
-
-    Route::post('upload/2', [TestController::class, 'uploadhappinessCenter']);
-
-    Route::post('upload/3', [TestController::class, 'uploadBalanceSheet']);
-
-    Route::post('upload/4', [TestController::class, 'uploadGeneralFund']);
-
-    Route::post('upload/5', [TestController::class, 'uploadReservedFund']);
-
-    Route::post('upload/6', [TestController::class, 'uploadBudgetVsActual']);
-
-    Route::post('upload/7', [TestController::class, 'uploadAccountsPayable']);
-
-    Route::post('upload/8', [TestController::class, 'uploadDelinquents']);
-
-    Route::post('upload/9', [TestController::class, 'uploadCollection']);
-
-    Route::post('upload/12', [TestController::class, 'uploadWorkOrders']);
-
-    Route::get('services-requests', [TestController::class, 'serviceRequest']);
-
-    Route::get('service-parameters', [TestController::class, 'serviceParameters']);
-
-    Route::post('upload-all', [TestController::class, 'uploadAll']);
-
-    Route::get('oa-service-details/{oaService}', [TestController::class, 'getOaService']);
