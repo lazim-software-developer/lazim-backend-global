956cb2f02fa7daedc0d90a669526fdb2ec1cc156
shilpaGowda <shilpa@zysk.tech>
2023-10-11 11:21:48 +0530

    feat: middleware for api validation
    Created a middleware for the apis wheere we cannot do authentication check

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'active' => \App\Http\Middleware\CheckUserActive::class,
+        'api.token' => \App\Http\Middleware\CheckApiToken::class,
     ];
 }
