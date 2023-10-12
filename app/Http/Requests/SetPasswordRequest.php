515fc154f2bda9f65b9ecbf202c5fbfb5436fc59
shilpaGowda <shilpa@zysk.tech>
2023-10-12 20:22:07 +0530

    feat: set password functionality
    added API for set password

---
+++ app/Http/Requests/SetPasswordRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SetPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required|string|min:8|confirmed',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'Email is required.',
+            'email.email' => 'Please enter a valid email address.',
+            'email.exists' => 'This email address is not registered.',
+            'password.required' => 'Password is required.',
+            'password.string' => 'Password should be a string.',
+            'password.min' => 'Password should be at least 8 characters.',
+            'password.confirmed' => 'Passwords do not match.',
+        ];
+    }
+}
