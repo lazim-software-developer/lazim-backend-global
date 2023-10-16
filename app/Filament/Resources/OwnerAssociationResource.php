3f092c877e87581fa9be9b1058ad1435a53b4912
Subhshreddy <subhash.reddy@zysk.tech>
2023-10-16 16:22:07 +0530

    UPDATE:applied validation to fields and removed creation button for OA

--- app/Filament/Resources/OwnerAssociationResource.php
+++ app/Filament/Resources/OwnerAssociationResource.php
@@ -31,6 +31,7 @@ class OwnerAssociationResource extends Resource
                     'lg' => 2,
                 ])->schema([
                     TextInput::make('name')
+                        ->rules(['regex:/^[a-zA-Z\s]*$/'])
                         ->required()
                         ->placeholder('User'),
                     TextInput::make('mollak_id')->label('Oa Number')
@@ -44,7 +45,7 @@ class OwnerAssociationResource extends Resource
 
                         ->placeholder('TRN Number'),
                     TextInput::make('phone')
-                        ->rules(['max:20', 'string'])
+                        ->rules(['digits:10', 'integer', 'regex:/^[6-9]\d{9}$/'])
                         ->required()
                         ->placeholder('Contact Number')
                         ->unique(
@@ -56,7 +57,7 @@ class OwnerAssociationResource extends Resource
                         ->required()
                         ->placeholder('Address'),
                     TextInput::make('email')
-                        ->rules(['max:50', 'string'])
+                        ->rules(['min:6', 'max:30', 'regex:/^[a-z0-9.]+@[a-z]+\.[a-z]{2,}$/'])
                         ->required()
                         ->disabled(function () {
                             return DB::table('owner_associations')
@@ -70,12 +71,7 @@ class OwnerAssociationResource extends Resource
                             fn(?Model $record) => $record
                         ),
                     Toggle::make('verified')
-                        ->rules(['boolean'])
-                        ->disabled(function () {
-                            return DB::table('owner_associations')
-                                ->where('verified', 1)
-                                ->exists();
-                        }),
+                        ->rules(['boolean']),
 
                 ]),
             ]);
