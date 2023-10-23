190e061feb554417da636b96cfc70a7cc712791b
shilpaGowda <shilpa@zysk.tech>
2023-10-23 12:54:47 +0530

    fix: add id and comments

--- app/Http/Resources/HelpDesk/Complaintresource.php
+++ app/Http/Resources/HelpDesk/Complaintresource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\HelpDesk;
 
+use App\Http\Resources\Community\CommentResource;
 use App\Http\Resources\MediaResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -16,13 +17,15 @@ class Complaintresource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'complaint' => $this->complaint,
             'categoty' => $this->category,
             'opened_on' => $this->open_time_diff,
             'resolved' => $this->status == 'open' ? false : true,
             'media' => MediaResource::collection($this->media),
             'complaint_type' => $this->complaint_type,
-            'complaint_details' => $this->complaint_details
+            'complaint_details' => $this->complaint_details,
+            'comments' => CommentResource::collection($this->whenLoaded('comments')),
         ];
     }
 }
