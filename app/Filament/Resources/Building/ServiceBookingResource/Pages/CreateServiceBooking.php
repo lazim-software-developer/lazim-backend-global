1e5d606e7ebfe5a0e6d7d00d046636ed87fee6a7
Subhshreddy <subhash.reddy@zysk.tech>
2023-10-25 16:54:38 +0530

    UPDATE: Implemented facility and service booking inside property Management.

---
+++ app/Filament/Resources/Building/ServiceBookingResource/Pages/CreateServiceBooking.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\Building\ServiceBookingResource\Pages;
+
+use App\Filament\Resources\Building\ServiceBookingResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateServiceBooking extends CreateRecord
+{
+    protected static string $resource = ServiceBookingResource::class;
+}
