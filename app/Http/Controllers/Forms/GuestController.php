a9cd5d0bba42293624f00bf89b601e2522879415
poojachakrawarti <pooja.chakrawarti@zysk.tech>
2023-10-30 15:43:33 +0530

    feat:Guest registration form

---
+++ app/Http/Controllers/Forms/GuestController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Forms;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Forms\CreateGuestRequest;
+use App\Http\Resources\CustomResponseResource;
+use App\Models\Building\Document;
+use App\Models\Forms\Guest;
+use App\Models\Visitor\FlatVisitor;
+
+class GuestController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(CreateGuestRequest $request)
+    {
+        $request->merge([
+            'start_time' => $request->start_date,
+            'end_time' => $request->end_date,
+            'initiated_by' => auth()->user()->id,
+        ]);
+        $guest = FlatVisitor::create($request->all());
+
+        $filePath = optimizeDocumentAndUpload($request->file('image'), 'dev');
+        $request->merge([
+            'flat_visitor_id'=> $guest->id,
+            'dtmc_license'=>$filePath,
+        ]);
+        Guest::create($request->all());
+
+        // Handle multiple images
+        if ($request->hasFile('files')) {
+            foreach ($request->file('files') as $image) {
+                $filePath = optimizeDocumentAndUpload($image, 'dev');
+                $currentDate = date('Y-m-d');
+
+                //TODO: NEED TO CHANGE EXPIRY_DATE LOGIC
+                $request->merge([
+                    'documentable_id' => $guest->id,
+                    'status' => 'pending',
+                    'url' => $filePath,
+                    'expiry_date' => date('Y-m-d', strtotime('+1 year', strtotime($currentDate))),
+                    'documentable_type' => FlatVisitor::class,
+                    'name' => $request->document_name,
+                ]);
+
+                Document::create($request->all());
+            }
+            return (new CustomResponseResource([
+                'title' => 'Success',
+                'message' => 'Move-IN created successfully!',
+                'errorCode' => 201,
+            ]))->response()->setStatusCode(201);
+        }
+    }
+}
--- database/seeders/DocumentLibrarySeeder.php
+++ database/seeders/DocumentLibrarySeeder.php
@@ -13,20 +13,20 @@ class DocumentLibrarySeeder extends Seeder
     public function run(): void
     {
         $doc = [
-            ['id' => 1, 'name' => 'Passport', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'master'],
-            ['id' => 2, 'name' => 'Visa', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'master'],
-            ['id' => 3, 'name' => 'Eid', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'master'],
-            ['id' => 4, 'name' => 'Title deed', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'master'],
-            ['id' => 5, 'name' => 'Acceptance from Developer', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'move-in'],
-            ['id' => 6, 'name' => 'Paid receipt of services', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'move-in'],
-            ['id' => 7, 'name' => 'Tenancy Contract', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'move-in'],
-            ['id' => 8, 'name' => 'DEWA', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'move-in'],
-            ['id' => 9, 'name' => 'Tenancy Contract', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'move-in'],
-            ['id' => 10, 'name' => 'Cooling Registration', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'move-in'],
-            ['id' => 11, 'name' => 'Gas Registration', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'move-in'],
-            ['id' => 12, 'name' => 'Vehicle Registration', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'move-in'],
-            ['id' => 13, 'name' => 'company license', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'move-in'],
-            ['id' => 14, 'name' => 'Security Deposit', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'lable'=>'move-in'],
+            ['id' => 1, 'name' => 'Passport', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'master'],
+            ['id' => 2, 'name' => 'Visa', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'master'],
+            ['id' => 3, 'name' => 'Eid', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'master'],
+            ['id' => 4, 'name' => 'Title deed', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'master'],
+            ['id' => 5, 'name' => 'Acceptance from Developer', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'move-in'],
+            ['id' => 6, 'name' => 'Paid receipt of services', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'move-in'],
+            ['id' => 7, 'name' => 'Tenancy Contract', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'move-in'],
+            ['id' => 8, 'name' => 'DEWA', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'move-in'],
+            ['id' => 9, 'name' => 'Tenancy Contract', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'move-in'],
+            ['id' => 10, 'name' => 'Cooling Registration', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'move-in'],
+            ['id' => 11, 'name' => 'Gas Registration', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'move-in'],
+            ['id' => 12, 'name' => 'Vehicle Registration', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'move-in'],
+            ['id' => 13, 'name' => 'company license', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'move-in'],
+            ['id' => 14, 'name' => 'Security Deposit', 'url'=>'dev/B5iVUw6NbQ4bSkss0cUQ3fmIRSippW-metaU2NyZWVuc2hvdCAoMTEpLnBuZw==-.png', 'type'=>'tenant', 'label'=>'move-in'],
         ];
         DocumentLibrary::insert($doc);
     }
