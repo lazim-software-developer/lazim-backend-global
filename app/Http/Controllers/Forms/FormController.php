8fca0a3e678a93e5f5a5f8a2c4ee187558665312
poojachakrawarti <pooja.chakrawarti@zysk.tech>
2023-10-30 11:27:41 +0530

    fixed: move-in-out API

--- app/Http/Controllers/Forms/FormController.php
+++ app/Http/Controllers/Forms/FormController.php
@@ -24,27 +24,16 @@ class FormController extends Controller
      */
     public function create(CreateFormRequest $request)
     {
-        Form::create([
-            'flat_id' => $request->flat_id,
-            'building_id' => $request->building_id,
-            'type' => $request->type,
-            'phone' =>  $request->phone,
-            'name' => $request->name,
-            'approved_id' => 2,
-            'moving_date' => $request->moving_date,
-            'moving_time' => $request->moving_time,
-            'preference' => $request->preference,
-            'email' => $request->email,
-        ]);
+        Form::create($request->all());
 
         // Handle multiple images
         if ($request->hasFile('images')) {
             foreach ($request->file('images') as $image) {
                 $filePath = optimizeDocumentAndUpload($image, 'dev');
                 $currentDate = date('Y-m-d');
-                Document::create([
-                    'document_library_id' => $request->document_library_id,
-                    'building_id' => $request->building_id,
+
+                //TODO: NEED TO CHANGE EXPIRY_DATE LOGIC
+                $request->merge([
                     'documentable_id' => auth()->user()->id,
                     'status' => 'submitted',
                     'url' => $filePath,
@@ -52,6 +41,8 @@ class FormController extends Controller
                     'documentable_type' => User::class,
                     'name' => $request->document_name,
                 ]);
+
+                Document::create($request->all());
             }
             return (new CustomResponseResource([
                 'title' => 'Success',
