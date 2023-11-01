46a7e61b6a4866e4a02c65b13b5bc4a158a4c223
Subhshreddy <subhash.reddy@zysk.tech>
2023-11-01 15:34:13 +0530

    UPDATE: implemented Resident form and Noc form.

---
+++ app/Filament/Resources/ResidentialFormResource/Pages/CreateResidentialForm.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ResidentialFormResource\Pages;
+
+use App\Filament\Resources\ResidentialFormResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateResidentialForm extends CreateRecord
+{
+    protected static string $resource = ResidentialFormResource::class;
+}
--- app/Models/ResidentialForm.php
+++ app/Models/ResidentialForm.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\Building\Building;
+use App\Models\Building\Flat;
+use App\Models\User\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
