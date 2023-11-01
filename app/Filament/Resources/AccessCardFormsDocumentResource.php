f68cda0ab6f0ea15155ac63748855278bd6bd532
poojachakrawarti <pooja.chakrawarti@zysk.tech>
2023-11-01 16:08:10 +0530

    Feat : Fit-out and Access card on Admin panel

---
+++ app/Filament/Resources/AccessCardFormsDocumentResource.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AccessCardFormsDocumentResource\Pages;
+use App\Models\Forms\AccessCard;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class AccessCardFormsDocumentResource extends Resource
+{
+    protected static ?string $model = AccessCard::class;
+
+    protected static ?string $modelLabel = 'AccessCard';
+    protected static ?string $navigationGroup = 'Forms Document';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('card_type')
+                    ->searchable()
+                    ->default('NA')
+                    ->limit(50),
+                TextColumn::make('email')
+                    ->searchable()
+                    ->default('NA')
+                    ->limit(50),
+                TextColumn::make('mobile')
+                    ->searchable()
+                    ->default('NA')
+                    ->limit(50),
+                TextColumn::make('reason')
+                    ->searchable()
+                    ->default('NA')
+                    ->limit(50),
+                TextColumn::make('parking_details')
+                    ->limit(50),
+                TextColumn::make('building.name')
+                    ->searchable()
+                    ->default('NA')
+                    ->limit(50),
+                TextColumn::make('user.first_name')
+                    ->searchable()
+                    ->default('NA')
+                    ->limit(50),
+                TextColumn::make('flat.property_number')
+                    ->searchable()
+                    ->default('NA')
+                    ->limit(50),
+                ImageColumn::make('passport')
+                    ->circular()
+                    ->disk('s3'),
+                ImageColumn::make('tenancy')
+                    ->circular()
+                    ->disk('s3'),
+                ImageColumn::make('vehicle_registration')
+                    ->label('Vehicle Registration')
+                    ->circular()
+                    ->disk('s3'),
+                TextColumn::make('status')
+                    ->searchable()
+                    ->default('NA')
+                    ->limit(50),
+                TextColumn::make('remarks')
+                    ->searchable()
+                    ->default('NA')
+                    ->limit(50),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                //Tables\Actions\EditAction::make(),
+                Action::make('Update Status')
+                    ->button()
+                    ->visible(fn (AccessCard $record): array => [
+                        'status' => function () use ($record) {
+                            if ($record->status === null) {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        },
+                    ])
+                    ->form([
+                        Select::make('status')
+                            ->options([
+                                'approved' => 'Approved',
+                                'rejected' => 'Rejected',
+                            ])
+                            ->searchable()
+                            ->live(),
+                        TextInput::make('remarks')
+                            ->rules(['max:255'])
+                            ->visible(function (callable $get) {
+                                if ($get('status') == 'rejected') {
+                                    return true;
+                                }
+                                return false;
+                            }),
+                    ])
+                    ->fillForm(fn (AccessCard $record): array => [
+                        'status' => $record->status,
+                        'remarks' => $record->remarks,
+                    ])
+                    ->action(function (AccessCard $record, array $data): void {
+                        if ($data['status'] == 'rejected') {
+                            $record->status = $data['status'];
+                            $record->remarks = $data['remarks'];
+                            $record->save();
+                        } else {
+                            $record->status = $data['status'];
+                            $record->save();
+                        }
+                    })
+                    ->slideOver()
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAccessCardFormsDocuments::route('/'),
+            'create' => Pages\CreateAccessCardFormsDocument::route('/create'),
+            'edit' => Pages\EditAccessCardFormsDocument::route('/{record}/edit'),
+        ];
+    }
+}
