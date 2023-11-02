281ba0fc3343bf491f5eb42a60b6890ba31fdca9
Subhshreddy <subhash.reddy@zysk.tech>
2023-11-02 16:08:33 +0530

    removed create option for fitout and accesscard.

--- app/Filament/Resources/AccessCardFormsDocumentResource/Pages/ListAccessCardFormsDocuments.php
+++ app/Filament/Resources/AccessCardFormsDocumentResource/Pages/ListAccessCardFormsDocuments.php
@@ -17,7 +17,7 @@ class ListAccessCardFormsDocuments extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            //Actions\CreateAction::make(),
         ];
     }
 }
