3006ad3f142deaef6a026856c0a29fe13d409a06
poojachakrawarti <pooja.chakrawarti@zysk.tech>
2023-11-07 16:08:33 +0530

    fixed reopened bugs

--- app/Filament/Resources/ComplaintsenquiryResource.php
+++ app/Filament/Resources/ComplaintsenquiryResource.php
@@ -35,104 +35,100 @@ class ComplaintsenquiryResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-        ->schema([
-            Grid::make([
-                'sm' => 1,
-                'md' => 1,
-                'lg' => 2])
-                ->schema([
-                    Hidden::make('complaintable_type')
-                        ->default('App\Models\Building\FlatTenant'),
-                    Hidden::make('complaintable_id')
-                        ->default(1),
-                    Hidden::make('owner_association_id')
-                        ->default(auth()->user()->owner_association_id),
-                    Select::make('building_id')
+            ->schema([
+                Grid::make([
+                    'sm' => 1,
+                    'md' => 1,
+                    'lg' => 2
+                ])
+                    ->schema([
+                        Hidden::make('complaintable_type')
+                            ->default('App\Models\Building\FlatTenant'),
+                        Hidden::make('complaintable_id')
+                            ->default(1),
+                        Hidden::make('owner_association_id')
+                            ->default(auth()->user()->owner_association_id),
+                        Select::make('building_id')
                             ->rules(['exists:buildings,id'])
                             ->relationship('building', 'name')
                             ->reactive()
                             ->preload()
                             ->searchable()
                             ->placeholder('Building'),
-                    Select::make('user_id')
-                        ->relationship('user','id')
-                        ->options(function(){
-                            $tenants = DB::table('flat_tenants')->pluck('tenant_id');
-                            // dd($tenants);
-                            return DB::table('users')
-                                ->whereIn('users.id',$tenants)
-                                ->select('users.id','users.first_name')
-                                ->pluck('users.first_name','users.id')
-                                ->toArray();
-                        })
-                        ->searchable()
-                        ->preload()
-                        ->required()
-                        ->label('User'),
-                    Select::make('category')
-                        ->options([
-                            'civil'    => 'Civil',
-                            'MIP'      => 'MIP',
-                            'security' => 'Security',
-                            'cleaning' => 'Cleaning',
-                            'others'   => 'Others',
-                        ])
-                        ->rules(['max:50', 'string'])
-                        ->searchable()
-                        ->required()
-                        ->placeholder('Category'),
-                    FileUpload::make('photo')
-                        ->disk('s3')
-                        ->directory('dev')
-                        ->maxSize(2048)
-                        ->image()
-                        ->nullable(),
-                    TextInput::make('complaint')
-                        ->placeholder('Enquiry'),
-                    TextInput::make('complaint_details')
-                        ->placeholder('Complaint Details'),
-                    Hidden::make('status')
-                        ->default('pending'),
-                    Hidden::make('complaint_type')
-                        ->default('enquiries'),
-                ])
-        ]);
+                        Select::make('user_id')
+                            ->relationship('user', 'id')
+                            ->options(function () {
+                                $tenants = DB::table('flat_tenants')->pluck('tenant_id');
+                                // dd($tenants);
+                                return DB::table('users')
+                                    ->whereIn('users.id', $tenants)
+                                    ->select('users.id', 'users.first_name')
+                                    ->pluck('users.first_name', 'users.id')
+                                    ->toArray();
+                            })
+                            ->searchable()
+                            ->preload()
+                            ->required()
+                            ->label('User'),
+                        Select::make('category')
+                            ->options([
+                                'civil'    => 'Civil',
+                                'MIP'      => 'MIP',
+                                'security' => 'Security',
+                                'cleaning' => 'Cleaning',
+                                'others'   => 'Others',
+                            ])
+                            ->rules(['max:50', 'string'])
+                            ->searchable()
+                            ->required()
+                            ->placeholder('Category'),
+                        FileUpload::make('photo')
+                            ->disk('s3')
+                            ->directory('dev')
+                            ->maxSize(2048)
+                            ->image()
+                            ->nullable(),
+                        TextInput::make('complaint')
+                            ->placeholder('Enquiry'),
+                        TextInput::make('complaint_details')
+                            ->placeholder('Complaint Details'),
+                        Hidden::make('status')
+                            ->default('open'),
+                        Hidden::make('complaint_type')
+                            ->default('enquiries'),
+                    ])
+            ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
-        ->columns([
-            TextColumn::make('building.name')
+            ->columns([
+                TextColumn::make('building.name')
                     ->default('NA')
                     ->searchable()
                     ->limit(50),
-            TextColumn::make('user.first_name')
-                ->toggleable()
-                ->default('NA')
-                ->searchable()
-                ->limit(50),
-            TextColumn::make('category')
-                ->toggleable()
-                ->default('NA')
-                ->searchable()
-                ->limit(50),
-            TextColumn::make('complaint')
-                ->toggleable()
-                ->default('NA')
-                ->searchable()
-                ->label('Enquiry'),
-            TextColumn::make('complaint_details')
-                ->toggleable()
-                ->default('NA')
-                ->searchable()
-                ->label('Complaint Details'),
-            TextColumn::make('status')
-                ->toggleable()
-                ->searchable()
-                ->limit(50),
+                TextColumn::make('user.first_name')
+                    ->toggleable()
+                    ->default('NA')
+                    ->searchable()
+                    ->limit(50),
+                TextColumn::make('complaint')
+                    ->toggleable()
+                    ->default('NA')
+                    ->searchable()
+                    ->label('Enquiry'),
+                TextColumn::make('complaint_details')
+                    ->toggleable()
+                    ->default('NA')
+                    ->searchable()
+                    ->label('Complaint Details'),
+                TextColumn::make('status')
+                    ->toggleable()
+                    ->searchable()
+                    ->limit(50),
 
-        ])
+            ])
             ->filters([
                 SelectFilter::make('building_id')
                     ->relationship('building', 'name')
@@ -177,14 +173,6 @@ class ComplaintsenquiryResource extends Resource
                         }
                     })
                     ->slideOver()
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ])
-            ->emptyStateActions([
-                Tables\Actions\CreateAction::make(),
             ]);
     }
 
@@ -199,8 +187,7 @@ class ComplaintsenquiryResource extends Resource
     {
         return [
             'index' => Pages\ListComplaintsenquiries::route('/'),
-            'create' => Pages\CreateComplaintsenquiry::route('/create'),
-            'edit' => Pages\EditComplaintsenquiry::route('/{record}/edit'),
+            'view' => Pages\ViewComplaintsenquiry::route('/{record}'),
         ];
     }
 }
