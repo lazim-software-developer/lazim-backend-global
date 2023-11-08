31c47a4928431915c688003cbd4eb5611542b47a
PrashanthZysk <prashanth.c@zysk.tech>
2023-11-08 19:51:07 +0530

    UPDATE: Document Upload Page Api

---
+++ app/Http/Controllers/Vendor/DocumentsUploadController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Vendor;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Vendor\DocumentsUploadRequest;
+use App\Models\Building\Document;
+use App\Models\Master\DocumentLibrary;
+use App\Models\Vendor\Vendor;
+use Illuminate\Http\Request;
+
+class DocumentsUploadController extends Controller
+{
+    public function documentsUpload(DocumentsUploadRequest $request)
+    {
+        
+        foreach($request->docs as $key => $value){
+            $path = optimizeDocumentAndUpload($value);
+            $request->merge([
+                'status'    => 'pending',
+                'documentable_type'   => Vendor::class,
+                'document_library_id' => DocumentLibrary::where('name', '=', $key)->first()->id,
+                'url' => $path,
+                'owner_association_id' => Vendor::find($request->documentable_id)->owner_association_id,
+            ]);
+            $document = Document::create($request->all());
+
+        }
+        return $document;
+    }
+}
---
+++ app/Http/Requests/Vendor/DocumentsUploadRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Vendor;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DocumentsUploadRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'documentable_id' =>'required|integer|exists:vendors,id',
+            'docs' => 'required|array',
+            'docs.tl_document' => 'required|file|max:2048',
+            'docs.tnr_certificate' => 'required|file|max:2048',
+            'docs.safety_policy'=> 'required|file|max:2048',
+            'docs.bank_details' => 'required|file|max:2048',
+            'docs.third_party_liability' => 'required|file|max:2048',
+            'docs.authority_approval' => 'nullable|file|max:2048',
+            'docs.risk_assessment' => 'nullable|file|max:2048',
+        ];
+    }
+}
