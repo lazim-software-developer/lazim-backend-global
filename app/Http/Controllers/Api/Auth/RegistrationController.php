098fde903d156a11a24897b14677cb020eb1e5a5
shilpaGowda <shilpa@zysk.tech>
2023-11-10 13:51:49 +0530

    fix: issue with registration
    - changed logic to check if email and phone is verified

--- app/Http/Controllers/Api/Auth/RegistrationController.php
+++ app/Http/Controllers/Api/Auth/RegistrationController.php
@@ -18,11 +18,13 @@ use App\Models\MollakTenant;
 use App\Models\User\User;
 use Illuminate\Support\Facades\DB;
 
-class RegisterationController extends Controller
+class RegistrationController extends Controller
 {
     public function registerWithEmailPhone(RegisterRequest $request) {
-        if(User::where(['email' => $request->email, 'phone' => $request->mobile])
-            ->where('email_verified', 0)->orWhere('phone_verified', 0)->exists()) {
+        
+        $userData = User::where(['email' => $request->get('email'), 'phone' => $request->get('mobile')]);
+        
+        if($userData->exists() && ($userData->first()->email_verified == 0 || $userData->first()->phone_verified == 0)) {
             return (new CustomResponseResource([
                 'title' => 'account_present',
                 'message' => "Your account is not verified. You'll be redirected account verification page",
