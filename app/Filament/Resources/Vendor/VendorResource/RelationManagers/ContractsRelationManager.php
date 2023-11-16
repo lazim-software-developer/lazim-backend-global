547c01af5d26e40afc2e7ddf7fe8ce70e5ef3b64
Subhshreddy <subhash.reddy@zysk.tech>
2023-11-16 18:46:09 +0530

    Implemented WDA and Invoice for vendors.

--- app/Filament/Resources/Vendor/VendorResource/RelationManagers/ContractsRelationManager.php
+++ app/Filament/Resources/Vendor/VendorResource/RelationManagers/ContractsRelationManager.php
@@ -2,19 +2,23 @@
 
 namespace App\Filament\Resources\Vendor\VendorResource\RelationManagers;
 
+use App\Models\Building\Building;
+use App\Models\Master\Service;
 use Filament\Forms;
-use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\DateTimePicker;
-use Filament\Forms\Components\FileUpload;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Illuminate\Support\Facades\DB;
+use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Form;
-use Filament\Resources\RelationManagers\RelationManager;
-use Filament\Tables;
-use Filament\Tables\Table;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\DateTimePicker;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\RelationManagers\RelationManager;
 
 class ContractsRelationManager extends RelationManager
 {
@@ -24,40 +28,55 @@ class ContractsRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                TextInput::make('contract_type')
-                    ->required()
-                    ->maxLength(255),
-                Select::make('building_id')
-                    ->rules(['exists:buildings,id'])
-                    ->relationship('building', 'name')
-                    ->reactive()
-                    ->preload()
-                    ->searchable()
-                    ->placeholder('Building'),
-                Select::make('service_id')
-                    ->rules(['exists:services,id'])
-                    ->relationship('service', 'name')
-                    ->reactive()
-                    ->preload()
-                    ->searchable()
-                    ->placeholder('Service'),
-                DatePicker::make('start_date')
-                    ->rules(['date'])
-                    ->required()
-                    ->placeholder('Start Date'),
-                DatePicker::make('end_date')
-                    ->rules(['date'])
-                    ->placeholder('End Date'),
-                FileUpload::make('document_url')
-                    ->disk('s3')
-                    ->directory('dev')
-                    ->openable(true)
-                    ->downloadable(true)
-                    ->label('Document'),
-                Hidden::make('vendor_id')
-                    ->default(function (RelationManager $livewire) {
-                        return $livewire->ownerRecord->id;
-                    }),
+                Grid::make([
+                    'sm' => 1,
+                    'md' => 1,
+                    'lg' => 2,
+                ])
+                    ->schema([
+                        Select::make('contract_type')
+                            ->options([
+                                'Annual Maintenance Contract' => 'Annual Maintenance Contract',
+                                'OneTime' => 'OneTime',
+                            ])
+                            ->searchable()
+                            ->required()
+                            ->label('Contract Type'),
+                        Select::make('building_id')
+                            ->options(function (RelationManager $livewire) {
+                                $buildingIds = DB::table('building_vendor')->where('vendor_id', $livewire->ownerRecord->id)->pluck('building_id')->toArray();
+                                return Building::whereIn('id', $buildingIds)->pluck('name', 'id')->toArray();
+                            })
+                            ->reactive()
+                            ->preload()
+                            ->searchable()
+                            ->placeholder('Building'),
+                        Select::make('service_id')
+                            ->options(function (RelationManager $livewire) {
+                                $serviceIds = DB::table('service_vendor')->where('vendor_id', $livewire->ownerRecord->id)->pluck('service_id')->toArray();
+                                return Service::whereIn('id', $serviceIds)->pluck('name', 'id')->toArray();
+                            })
+                            ->reactive()
+                            ->preload()
+                            ->searchable()
+                            ->placeholder('Service'),
+                        DatePicker::make('start_date')
+                            ->rules(['date'])
+                            ->placeholder('Start Date'),
+                        DatePicker::make('end_date')
+                            ->rules(['date'])
+                            ->placeholder('End Date'),
+                        FileUpload::make('document_url')
+                            ->disk('s3')
+                            ->directory('dev')
+                            ->openable(true)
+                            ->downloadable(true)
+                            ->label('Document'),
+                        Hidden::make('vendor_id')
+                            ->default(function (RelationManager $livewire) {
+                                return $livewire->ownerRecord->id;
+                            }),
+                    ])
             ]);
     }
 
--- app/Models/Accounting/WDA.php
+++ app/Models/Accounting/WDA.php
@@ -2,12 +2,44 @@
 
 namespace App\Models\Accounting;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User\User;
+use App\Models\Vendor\Vendor;
+use App\Models\Vendor\Contract;
+use App\Models\Building\Building;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class WDA extends Model
 {
     use HasFactory;
 
     protected $table = 'wda';
+    protected $fillable = [
+        'date',
+        'job_description',
+        'document',
+        'created_by',
+        'status',
+        'remarks',
+        'building_id',
+        'contract_id',
+        'status_updated_by',
+        'vendor_id',
+    ];
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
+    public function building()
+    {
+        return $this->belongsTo(Building::class);
+    }
+    public function contract()
+    {
+        return $this->belongsTo(Contract::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class,'status_updated_by');
+    }
 }
