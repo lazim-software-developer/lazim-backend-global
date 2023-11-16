55d45ccb89891075541b856aa707f9fb162b59b6
Subhshreddy <subhash.reddy@zysk.tech>
2023-11-16 12:50:44 +0530

    removed create option.

--- app/Filament/Resources/MoveInFormsDocumentResource/Pages/ListMoveInFormsDocuments.php
+++ app/Filament/Resources/MoveInFormsDocumentResource/Pages/ListMoveInFormsDocuments.php
@@ -18,7 +18,7 @@ class ListMoveInFormsDocuments extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            //Actions\CreateAction::make(),
         ];
     }
 }
