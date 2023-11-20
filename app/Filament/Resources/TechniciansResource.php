333bf4e6fb41d9fa1211896681f493c0551dd076
Subhshreddy <subhash.reddy@zysk.tech>
2023-11-20 15:18:00 +0530

    implemented technicians asset feature.

---
+++ app/Filament/Resources/TechniciansResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use App\Models\User\User;
+use Filament\Tables\Table;
+use App\Models\Technicians;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Grid;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\TechniciansResource\Pages;
+use App\Filament\Resources\TechniciansResource\RelationManagers;
+
+class TechniciansResource extends Resource
+{
+    protected static ?string $model = User::class;
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Vendor Management';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Grid::make([
+                    'sm' => 1,
+                    'md' => 1,
+                    'lg' => 2,
+                ])
+                    ->schema([
+                        //
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTechnicians::route('/'),
+            'create' => Pages\CreateTechnicians::route('/create'),
+            'edit' => Pages\EditTechnicians::route('/{record}/edit'),
+        ];
+    }
+}
