93ed67647677e95e8d4a3361dd10426ebf38cfff
PrashanthZysk <prashanth.c@zysk.tech>
2023-11-20 15:58:02 +0530

    UPDATE: Remaining days in Vendor Contracts

--- app/Http/Resources/Vendor/ContractResource.php
+++ app/Http/Resources/Vendor/ContractResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\Vendor;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
 
 class ContractResource extends JsonResource
 {
@@ -14,11 +15,14 @@ class ContractResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $startDate = Carbon::parse($this->start_date);
+        $endDate = Carbon::parse($this->end_date);
         return [
             'id'  => $this->id,
             'contract'=> $this->contract_type,
-            'start_date' => $this->start_date,
-            'end_date'=> $this->end_date,
+            'start_date' => $startDate->format('Y-m-d'), 
+            'end_date' => $endDate->format('Y-m-d'), 
+            'days_remaining' => Carbon::now()->diffInDays($endDate),
             'url'   => env('AWS_URL').'/'.$this->document_url,
 
         ];
