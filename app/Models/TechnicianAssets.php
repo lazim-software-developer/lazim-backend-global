2f75561b000d8223af3cee241c8f4f257a09ae76
shilpaGowda <shilpa@zysk.tech>
2023-11-20 15:15:57 +0530

    fix: wip apis to list buildinga assets

--- app/Models/TechnicianAssets.php
+++ app/Models/TechnicianAssets.php
@@ -2,18 +2,29 @@
 
 namespace App\Models;
 
+use App\Models\Building\Building;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TechnicianAssets extends Model
 {
     use HasFactory;
+
     protected $table = 'technician_assets';
+
     protected $fillable = [
         'technician_id',
         'vendor_id',
         'asset_id',
         'active',
+        'building_id'
     ];
 
+    public function asset() {
+        return $this->belongsTo(Asset::class);
+    }
+
+    public function building() {
+        return $this->belongsTo(Building::class);
+    }
 }
