338380736d11166e973e34d179bf0baadb529a68
shilpaGowda <shilpa@zysk.tech>
2023-11-21 12:12:58 +0530

    feat: get api for asset maintenance list

--- app/Http/Controllers/Assets/AssetController.php
+++ app/Http/Controllers/Assets/AssetController.php
@@ -4,14 +4,12 @@ namespace App\Http\Controllers\Assets;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Assets\StoreAssetMaintenanceRequest;
-use App\Http\Requests\Assets\UpdateAssetMaintenanceAfterRequest;
 use App\Http\Requests\Assets\UpdateAssetMaintenanceBeforeRequest;
-use App\Http\Resources\Assets\TechnicianAssetResource;
+use App\Http\Resources\Assets\AssetMaintenanceResource;
 use App\Http\Resources\CustomResponseResource;
 use App\Models\Assets\Assetmaintenance;
 use App\Models\TechnicianAssets;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 
 class AssetController extends Controller
 {
@@ -121,32 +119,12 @@ class AssetController extends Controller
         ]))->response()->setStatusCode(200);
     }
 
-    public function updateAfter(UpdateAssetMaintenanceAfterRequest $request, AssetMaintenance $assetMaintenance)
-    {
-        $imagePath = optimizeAndUpload($request->media, 'dev');
+    // List all entries for an asset
+    public function fetchAssetMaintenances(TechnicianAssets $technicianasset) {
+        $assets = Assetmaintenance::where(
+            ['technician_asset_id' => $technicianasset->id, 'maintained_by' => auth()->user()->id]
+        )->latest()->paginate();
 
-        // Fetch and decode the existing JSON data
-        $commentData = json_decode($assetMaintenance->comment, true);
-        $mediaData = json_decode($assetMaintenance->media, true);
-
-        // Update the 'after' part for comment
-        $commentData['after'] = $request->input('comment');
-        $mediaData['after'] = $imagePath;
-
-        $request->merge([
-            'status' => 'completed'
-        ]);
-
-        $assetMaintenance->update([
-            'comment' => json_encode($commentData),
-            'media' => json_encode($mediaData),
-            'status' => 'completed'
-        ]);
-
-        return (new CustomResponseResource([
-            'title' => 'Success',
-            'message' => 'Record updated successfully!',
-            'code' => 200,
-        ]))->response()->setStatusCode(200);
+        return AssetMaintenanceResource::collection($assets);
     }
 }
