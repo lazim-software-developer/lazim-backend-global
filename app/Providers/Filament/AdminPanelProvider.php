8848fe8224a6094a600aa29666eb0f30b5adce4a
shilpaGowda <shilpa@zysk.tech>
2023-11-23 13:30:29 +0530

    fix: UI fixes in creat tender page

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,6 +40,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Indigo,
                 'success' => Color::Emerald,
                 'warning' => Color::Orange,
+                'pink' => Color::Pink
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
