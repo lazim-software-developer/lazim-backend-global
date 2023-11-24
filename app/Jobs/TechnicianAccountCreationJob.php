d1b288664ea6645082583f560c08316c37d92bec
PrashanthZysk <prashanth.c@zysk.tech>
2023-11-24 16:04:53 +0530

    FIX: technician Registeration Mail

---
+++ app/Jobs/TechnicianAccountCreationJob.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Snowfire\Beautymail\Beautymail;
+
+class TechnicianAccountCreationJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public $user;
+    public $password;
+
+    public function __construct($user, $password)
+    {
+        $this->user     = $user;
+        $this->password = $password;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        $beautymail = app()->make(Beautymail::class);
+        $beautymail->send('emails.technician_registration', ['user' => $this->user, 'password' => $this->password], function($message) {
+            $message
+                ->to($this->user->email, $this->user->first_name)
+                ->subject('Welcome to Lazim!');
+        });
+    }
+}
