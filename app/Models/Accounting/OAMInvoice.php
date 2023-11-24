ca66e26b9bf8b4de9dca889eb687b44e2f822280
shilpaGowda <shilpa@zysk.tech>
2023-11-24 11:17:35 +0530

    feat: setup for delinquint owners

---
+++ app/Models/Accounting/OAMInvoice.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models\Accounting;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class OAMInvoice extends Model
+{
+    use HasFactory;
+
+    protected $table = 'oam_invoices';
+
+    protected $fillable = [
+        'building_id',
+        'flat_id',
+        'invoice_number',
+        'invoice_date',
+        'invoice_status',
+        'due_amount',
+        'general_fund_amount',
+        'reserve_fund_amount',
+        'additional_charges',
+        'previous_balance',
+        'adjust_amount',
+        'invoice_due_date',
+        'invoice_pdf_link',
+        'invoice_detail_link',
+        'updated_by',
+    ];
+
+    // Save data to invoice audit table before updating the entry
+    protected static function booted()
+    {
+        static::updating(function ($oamInvoice) {
+            DB::transaction(function () use ($oamInvoice) {
+                // Capture current data
+                $originalData = $oamInvoice->getOriginal();
+
+                // Save to audit table
+                DB::table('oam_invoice_audits')->insert([
+                    'oam_invoice_id' => $oamInvoice->id,
+                    'data' => json_encode($originalData),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            });
+        });
+    }
+}
