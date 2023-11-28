63115a894906390a295c89b9dad0eaa1670462ae
shilpaGowda <shilpa@zysk.tech>
2023-11-28 19:00:09 +0530

    feat: create proposal api

--- app/Http/Controllers/Vendor/TenderController.php
+++ app/Http/Controllers/Vendor/TenderController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Vendor;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CustomResponseResource;
 use App\Http\Resources\Vendor\ContractResource;
 use App\Http\Resources\Vendor\TenderResource;
+use App\Models\Accounting\Proposal;
 use App\Models\Accounting\Tender;
 use App\Models\Vendor\Contract;
 use App\Models\Vendor\Vendor;
@@ -24,5 +26,34 @@ class TenderController extends Controller
         $tenders = Tender::whereIn('id', $tendersIds)->paginate(10);
         
         return TenderResource::collection($tenders);
+    }
+
+    // Create proposal
+    public function store(Tender $tender, Request $request) {
+
+        $filePath = optimizeDocumentAndUpload($request->file('file'), 'dev');
+
+        $vendor = Vendor::where('owner_id', auth()->user()->id)->first();
+
+        Proposal::create([
+            'tender_id' => $tender->id,
+            'amount' => $request->amount,
+            'submitted_by' => auth()->user()->id,
+            'submitted_on' => now(),
+            'document' => $filePath
+        ]);
+
+        DB::table('tender_vendors')->where([
+            'tender_id' => $tender->id,
+            'vendor_id' => $vendor->id
+        ])->update([
+            'status' => 'approved'
+        ]);
+
+        return (new CustomResponseResource([
+            'title' => 'Success',
+            'message' => 'Proposal created successfully!',
+            'code' => 201,
+        ]))->response()->setStatusCode(201);
     }
 }
