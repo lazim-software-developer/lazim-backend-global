3440572320261335fa7bcf9b9dc6fdb67e667a82
PrashanthZysk <prashanth.c@zysk.tech>
2023-11-29 11:51:31 +0530

    UPDATE: Ledger Table filters

--- app/Filament/Resources/VendorLedgersResource.php
+++ app/Filament/Resources/VendorLedgersResource.php
@@ -5,16 +5,21 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\VendorLedgersResource\Pages;
 use App\Filament\Resources\VendorLedgersResource\RelationManagers;
 use App\Models\Accounting\Invoice;
+use App\Models\Building\Building;
 use App\Models\VendorLedgers;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Malzariey\FilamentDaterangepickerFilter\Fields\DateRangePicker;
 
 class VendorLedgersResource extends Resource
 {
@@ -59,8 +64,50 @@ class VendorLedgersResource extends Resource
                     ->searchable(),
             ])
             ->filters([
-                //
-            ])
+                Filter::make('date')
+                            ->form([
+                                DateRangePicker::make('Date')
+                            ])
+                            ->query(function (Builder $query, array $data): Builder {
+                                if (isset($data['Date'])) {
+                                    $dateRange = explode(' - ', $data['Date']);
+                            
+                                    if (count($dateRange) === 2) {
+                                        $from = \Carbon\Carbon::createFromFormat('d/m/Y', $dateRange[0])->format('Y-m-d');
+                                        $until = \Carbon\Carbon::createFromFormat('d/m/Y', $dateRange[1])->format('Y-m-d');
+                            
+                                        return $query
+                                            ->when(
+                                                $from,
+                                                fn (Builder $query, $date) => $query->whereDate('date', '>=', $date)
+                                            )
+                                            ->when(
+                                                $until,
+                                                fn (Builder $query, $date) => $query->whereDate('date', '<=', $date)
+                                            );
+                                    }
+                                }
+                                
+                                    return $query;
+                                }),
+                        Filter::make('Building')
+                            ->form([
+                                Select::make('building')
+                                ->searchable()
+                                ->options(function () {
+                                    $oaId = auth()->user()->owner_association_id;
+                                    return Building::where('owner_association_id', $oaId)
+                                        ->pluck('name', 'id');
+                                })
+                            ])
+                            ->query(function (Builder $query, array $data): Builder {
+                                return $query
+                                    ->when(
+                                        $data['building'],
+                                        fn (Builder $query, $building_id): Builder => $query->where('building_id', $building_id),
+                                    );
+                                }),
+                            ],layout: FiltersLayout::AboveContent)->filtersFormColumns(3)
             ->actions([
                 // Tables\Actions\EditAction::make(),
             ])
