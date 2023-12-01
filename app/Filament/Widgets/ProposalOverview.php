2e7ced67220f07902280d03470c5f170028629e1
Subhshreddy <subhash.reddy@zysk.tech>
2023-12-01 18:46:33 +0530

    Implemented dashboard.

---
+++ app/Filament/Widgets/ProposalOverview.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Vendor\Vendor;
+use App\Models\Accounting\Proposal;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+
+class ProposalOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $vendorIds = Vendor::all()->where('owner_association_id', auth()->user()->owner_association_id)->pluck('id')->toArray();
+        return [
+            Stat::make('Request Proposal', Proposal::query()->whereIn('vendor_id', $vendorIds)->count())
+                ->descriptionIcon('heroicon-s-user-group')
+                ->chart([60, 92, 33, 80, 31, 98, 70])
+                ->color('info'),
+            Stat::make('Approved Proposal', Proposal::query()->whereIn('vendor_id', $vendorIds)->where('status','approved')->count())
+                ->descriptionIcon('heroicon-s-user-group')
+                ->chart([60, 92, 33, 80, 31, 98, 70])
+                ->color('success'),
+            Stat::make('Rejected Proposal', Proposal::query()->whereIn('vendor_id', $vendorIds)->where('status','rejected')->count())
+                ->descriptionIcon('heroicon-s-user-group')
+                ->chart([60, 92, 33, 80, 31, 98, 70])
+                ->color('danger'),
+        ];
+    }
+}
