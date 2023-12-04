fb87ffdac2f95a4a81a2f234a3098d1901268ba1
Subhshreddy <subhash.reddy@zysk.tech>
2023-12-04 16:34:52 +0530

    security for a building with email notification.

---
+++ app/Jobs/BuildingSecurity.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Snowfire\Beautymail\Beautymail;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class BuildingSecurity implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public $user;
+    public $password;
+    public function __construct($user, $password)
+    {
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $beautymail = app()->make(Beautymail::class);
+        $beautymail->send('emails.buildingsecurity', ['user' => $this->user, 'password' => $this->password], function ($message) {
+            $message
+                ->to($this->user->email, $this->user->first_name)
+                ->subject('Welcome to Lazim!');
+        });
+    }
+}
