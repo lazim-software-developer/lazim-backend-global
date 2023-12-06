96667b9731d87131257c87291dd99f03936269cd
poojachakrawarti <pooja.chakrawarti@zysk.tech>
2023-12-06 18:29:05 +0530

    generating contract from admin side

--- app/Filament/Resources/MediaResource/Pages/ListMedia.php
+++ app/Filament/Resources/MediaResource/Pages/ListMedia.php
@@ -13,7 +13,7 @@ class ListMedia extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
