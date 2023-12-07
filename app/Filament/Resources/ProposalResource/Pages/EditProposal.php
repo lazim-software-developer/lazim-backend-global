103517dbcc90e01560b95763652975b8dbf01b83
poojachakrawarti <pooja.chakrawarti@zysk.tech>
2023-12-07 12:42:57 +0530

    proposal edit action

--- app/Filament/Resources/ProposalResource/Pages/EditProposal.php
+++ app/Filament/Resources/ProposalResource/Pages/EditProposal.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\ProposalResource\Pages;
 
 use App\Filament\Resources\ProposalResource;
-use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
 class EditProposal extends EditRecord
