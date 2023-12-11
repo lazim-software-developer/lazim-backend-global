7f462550fe2d936a3e18fc46feb4864c9e240903
Subhshreddy <subhash.reddy@zysk.tech>
2023-12-11 19:43:31 +0530

    ....

--- app/Filament/Resources/PostResource/RelationManagers/CommentsRelationManager.php
+++ app/Filament/Resources/PostResource/RelationManagers/CommentsRelationManager.php
@@ -3,12 +3,13 @@
 namespace App\Filament\Resources\PostResource\RelationManagers;
 
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Forms\Components\Select;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\RelationManagers\RelationManager;
 
 class CommentsRelationManager extends RelationManager
 {
@@ -20,7 +21,26 @@ class CommentsRelationManager extends RelationManager
             ->schema([
                 Forms\Components\TextInput::make('body')
                     ->required()
-                    ->maxLength(255),
+                    ->maxLength(255)
+                    ->label('Comment')
+                    ->disabled()
+                    ->columnSpan([
+                        'sm' => 1,
+                        'md' => 1,
+                        'lg' => 2,
+                    ]),
+                    Select::make('user_id')
+                    ->relationship('user', 'first_name')
+                    ->searchable()
+                    ->preload()
+                    ->required()
+                    ->label('User')
+                    ->columnSpan([
+                        'sm' => 1,
+                        'md' => 1,
+                        'lg' => 2,
+                    ]),
+
             ]);
     }
 
@@ -29,7 +49,8 @@ class CommentsRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('body')
             ->columns([
-                Tables\Columns\TextColumn::make('body'),
+                Tables\Columns\TextColumn::make('body')->label('Comment'),
+                Tables\Columns\TextColumn::make('user.first_name'),
             ])
             ->filters([
                 //
@@ -38,7 +59,7 @@ class CommentsRelationManager extends RelationManager
                 // Tables\Actions\CreateAction::make(),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
