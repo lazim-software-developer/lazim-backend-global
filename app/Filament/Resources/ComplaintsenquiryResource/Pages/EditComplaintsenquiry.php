46c9316ede3127771a36cfcc9ffb5bad4f5f2149
poojachakrawarti <pooja.chakrawarti@zysk.tech>
2023-12-14 11:46:06 +0530

    Fixed push notification for mobile app

--- app/Filament/Resources/ComplaintsenquiryResource/Pages/EditComplaintsenquiry.php
+++ app/Filament/Resources/ComplaintsenquiryResource/Pages/EditComplaintsenquiry.php
@@ -3,11 +3,15 @@
 namespace App\Filament\Resources\ComplaintsenquiryResource\Pages;
 
 use App\Filament\Resources\ComplaintsenquiryResource;
+use App\Models\ExpoPushNotification;
+use App\Traits\UtilsTrait;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\DB;
 
 class EditComplaintsenquiry extends EditRecord
 {
+    use UtilsTrait;
     protected static string $resource = ComplaintsenquiryResource::class;
 
     protected function getHeaderActions(): array
@@ -15,5 +19,43 @@ class EditComplaintsenquiry extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    public function afterSave()
+    {
+        if ($this->record->status == 'closed') {
+            $expoPushTokens = ExpoPushNotification::where('user_id', $this->record->user_id)->pluck('token');
+            if ($expoPushTokens->count() > 0) {
+                foreach ($expoPushTokens as $expoPushToken) {
+                    $message = [
+                        'to' => $expoPushToken,
+                        'sound' => 'default',
+                        'title' => 'Enquiry Acknowledgement',
+                        'body' => 'You enquiry has been acknowledged by '.auth()->user()->first_name. '. Team will contact you soon.',
+                        'data' => ['notificationType' => 'app_notification'],
+                    ];
+                    $this->expoNotification($message);
+                    DB::table('notifications')->insert([
+                        'id' => (string) \Ramsey\Uuid\Uuid::uuid4(),
+                        'type' => 'Filament\Notifications\DatabaseNotification',
+                        'notifiable_type' => 'App\Models\User\User',
+                        'notifiable_id' => $this->record->user_id,
+                        'data' => json_encode([
+                            'actions' => [],
+                            'body' => 'You enquiry has been acknowledged by '.auth()->user()->first_name. '. Team will contact you soon.',
+                            'duration' => 'persistent',
+                            'icon' => 'heroicon-o-document-text',
+                            'iconColor' => 'warning',
+                            'title' => 'Enquiry Acknowledgement',
+                            'view' => 'notifications::notification',
+                            'viewData' => [],
+                            'format' => 'filament'
+                        ]),
+                        'created_at' => now()->format('Y-m-d H:i:s'),
+                        'updated_at' => now()->format('Y-m-d H:i:s'),
+                    ]);
+                }
+            }
+        }
     }
 }
