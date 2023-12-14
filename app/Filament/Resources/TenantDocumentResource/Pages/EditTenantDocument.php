12cb614da5c2bf2a4e29e9e344d666fbc7e3b990
poojachakrawarti <pooja.chakrawarti@zysk.tech>
2023-12-14 12:58:32 +0530

    fixed documents push notification

--- app/Filament/Resources/TenantDocumentResource/Pages/EditTenantDocument.php
+++ app/Filament/Resources/TenantDocumentResource/Pages/EditTenantDocument.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\TenantDocumentResource\Pages;
 
 use App\Filament\Resources\TenantDocumentResource;
 use App\Models\Building\Document;
+use App\Models\ExpoPushNotification;
 use App\Models\User\User;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
@@ -23,11 +24,80 @@ class EditTenantDocument extends EditRecord
     public function afterSave()
     {
         // If updated value of status is approved
-        if($this->record->status == 'approved') {
+        if ($this->record->status == 'approved') {
             Document::where('id', $this->data['id'])
                 ->update([
                     'accepted_by' => auth()->user()->id,
                 ]);
+
+            $expoPushTokens = ExpoPushNotification::where('user_id', $this->record->documentable_id)->pluck('token');
+            if ($expoPushTokens->count() > 0) {
+                foreach ($expoPushTokens as $expoPushToken) {
+                    $message = [
+                        'to' => $expoPushToken,
+                        'sound' => 'default',
+                        'title' => $this->record->name . ' Submission Status',
+                        'body' => 'Your Access card form has been approved.',
+                        'body' => 'The document ' . $this->record->name . 'submitted by you has been ' . $this->record->status . ' by OA admin.',
+                        'data' => ['notificationType' => 'app_notification'],
+                    ];
+                    $this->expoNotification($message);
+                    DB::table('notifications')->insert([
+                        'id' => (string) \Ramsey\Uuid\Uuid::uuid4(),
+                        'type' => 'Filament\Notifications\DatabaseNotification',
+                        'notifiable_type' => 'App\Models\User\User',
+                        'notifiable_id' => $this->record->user_id,
+                        'data' => json_encode([
+                            'actions' => [],
+                            'body' => 'Your Access card form has been approved. ',
+                            'duration' => 'persistent',
+                            'icon' => 'heroicon-o-document-text',
+                            'iconColor' => 'warning',
+                            'title' => $this->record->name . ' Submission Status',
+                            'view' => 'notifications::notification',
+                            'viewData' => [],
+                            'format' => 'filament'
+                        ]),
+                        'created_at' => now()->format('Y-m-d H:i:s'),
+                        'updated_at' => now()->format('Y-m-d H:i:s'),
+                    ]);
+                }
+            }
+        }
+
+        if ($this->record->status == 'rejected') {
+            $expoPushTokens = ExpoPushNotification::where('user_id', $this->record->documentable_id)->pluck('token');
+            if ($expoPushTokens->count() > 0) {
+                foreach ($expoPushTokens as $expoPushToken) {
+                    $message = [
+                        'to' => $expoPushToken,
+                        'sound' => 'default',
+                        'title' => $this->record->name . ' Submission Status',
+                        'body' => 'The document ' . $this->record->name . 'submitted by you has been ' . $this->record->status . ' by OA admin.',
+                        'data' => ['notificationType' => 'app_notification'],
+                    ];
+                    $this->expoNotification($message);
+                    DB::table('notifications')->insert([
+                        'id' => (string) \Ramsey\Uuid\Uuid::uuid4(),
+                        'type' => 'Filament\Notifications\DatabaseNotification',
+                        'notifiable_type' => 'App\Models\User\User',
+                        'notifiable_id' => $this->record->user_id,
+                        'data' => json_encode([
+                            'actions' => [],
+                            'body' => 'The document ' . $this->record->name . 'submitted by you has been ' . $this->record->status . ' by OA admin.',
+                            'duration' => 'persistent',
+                            'icon' => 'heroicon-o-document-text',
+                            'iconColor' => 'danger',
+                            'title' => $this->record->name . ' Submission Status',
+                            'view' => 'notifications::notification',
+                            'viewData' => [],
+                            'format' => 'filament'
+                        ]),
+                        'created_at' => now()->format('Y-m-d H:i:s'),
+                        'updated_at' => now()->format('Y-m-d H:i:s'),
+                    ]);
+                }
+            }
         }
     }
 }
