32c8b3c5d6850990b9a914e15d1596bd75a8f295
shilpaGowda <shilpa@zysk.tech>
2023-12-15 16:03:25 +0530

    fix: changes to API

--- app/Http/Controllers/Gatekeeper/ComplaintController.php
+++ app/Http/Controllers/Gatekeeper/ComplaintController.php
@@ -14,23 +14,12 @@ class ComplaintController extends Controller
 {
     public function index()
     {
-        // Check if the gatekeeper is having active account inuildingPOC table
-        $building = BuildingPoc::where([
+        // Fetch active buildign id
+        $buildingId = BuildingPoc::where([
             'user_id' => auth()->user()->id,
             'role_name' => 'security',
             'active' => 1
-        ]);
-
-        if(!$building->exists()) {
-            return (new CustomResponseResource([
-                'title' => 'Error',
-                'message' => "You don't have access to login to the application!",
-                'code' => 403,
-            ]))->response()->setStatusCode(403);
-        }
-
-        // Get the building_id
-        $buildingId = $building->first()->building_id;
+        ])->value('building_id');
 
         // Fetch al the complaints which are not resolved for the building where service_id is security related services
 
