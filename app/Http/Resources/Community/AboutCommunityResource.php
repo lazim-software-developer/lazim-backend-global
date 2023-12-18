73833d695b65301edb278b24cd1d79302bf7ec8b
shilpaGowda <shilpa@zysk.tech>
2023-12-18 16:20:33 +0530

    fix: changes to about community api

--- app/Http/Resources/Community/AboutCommunityResource.php
+++ app/Http/Resources/Community/AboutCommunityResource.php
@@ -15,29 +15,30 @@ class AboutCommunityResource extends JsonResource
     public function toArray(Request $request): array
     {
         $facilities = $this->facilities->unique();
-        $services = $this->services->where('type','inhouse')->unique();
+        $services = $this->services->where('type', 'inhouse')->unique();
+
         $facilitiesData = $facilities->map(function ($facility) {
             return [
                 'id' => $facility->id,
                 'name' => $facility->name,
-                'icon' => env('AWS_URL').'/'.$facility->icon,
+                'icon' => env('AWS_URL') . '/' . $facility->icon,
             ];
         });
-        
+
         $servicesData = $services->map(function ($service) {
             return [
                 'id' => $service->id,
                 'name' => $service->name,
-                'icon' => env('AWS_URL').'/'.$service->icon,
+                'icon' => env('AWS_URL') . '/' . $service->icon,
             ];
         });
-        
+
         return [
-            "image" => env('AWS_URL').'/'.'dev/images/amenity.jpg',
+            "image" => env('AWS_URL') . '/' . 'dev/images/amenity.jpg',
             "about" => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sint asperiores sapiente voluptatum, atque delectus facere repellendus dolor esse, temporibus nesciunt iure ratione consequuntur, fugit numquam recusandae id nulla rerum iusto. Unde delectus quos fuga ad officiis fugit facilis nam quas recusandae temporibus perferendis tempora id quibusdam illo, a aperiam praesentium totam obcaecati molestias similique exercitationem. Veritatis aliquid ipsam similique doloribus alias, saepe reiciendis quaerat expedita, modi perspiciatis, perferendis accusantium nostrum esse laborum eaque soluta quidem rerum autem magnam odit eos sint incidunt ducimus. Molestiae maxime hic tempora voluptas accusamus sed! Unde corrupti facere dolorem suscipit harum dolore, nemo illum.',
             "facilities" => $facilitiesData,
             "services" => $servicesData,
-            "slug" => 'SYMBIOSIS'
+            "slug" => $this->slug,
         ];
     }
 }
