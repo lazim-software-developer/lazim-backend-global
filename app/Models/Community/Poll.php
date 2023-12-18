54d54c3c5a7a9feb284f452ecb5a4a98be316454
shilpaGowda <shilpa@zysk.tech>
2023-12-18 14:22:53 +0530

    feat: polling feature for a building
    - A get API to list all polls for a building
    - Added stats in the get API for each options
    - A post API to submit poll response

---
+++ app/Models/Community/Poll.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models\Community;
+
+use App\Models\Building\Building;
+use App\Models\User\User;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Poll extends Model
+{
+    use HasFactory;
+
+    protected $table = 'polls';
+
+    protected $fillable = ['question', 'options', 'status', 'scheduled_at', 'ends_on', 'building_id', 'created_by'];
+
+    protected $casts = ['options' => 'array'];
+
+    protected $dates = ['scheduled_at'];
+
+    public function getEndsOnDiffAttribute()
+    {
+        return Carbon::parse($this->attributes['ends_on'])->diffForHumans();
+    }
+
+    public function building()
+    {
+        return $this->belongsTo(Building::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function responses() {
+        return $this->hasMany(PollResponse::class);
+    }
+}
