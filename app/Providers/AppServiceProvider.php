477ffc536e2c8b3d2c08ac2ce32594e59ffa58ae
Pooja Chakrawarti <pooja.chakrawarti@zysk.tech>
2023-12-18 18:45:58 +0530

    vendor create notification to OA admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Accounting\Proposal;
 use App\Models\Accounting\WDA;
 use App\Models\Building\Building;
 use App\Models\Building\Complaint;
@@ -72,7 +73,7 @@ class AppServiceProvider extends ServiceProvider
         FacilityBooking::observe(FacilityServiceBookingObserver::class);
         PostLike::observe(PostLikeObserver::class);
         User::observe(UserObserver::class);
-        // Proposal::observe(ProposalObserver::class);
+        Proposal::observe(ProposalObserver::class);
         WDA::observe(WDAObserver::class);
         TechnicianAssets::observe(TechnicianAssetsObserver::class);
     }
