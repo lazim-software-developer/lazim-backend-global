ff81e51c8fde175204f04edb9e129db8a518a1e2
Pooja <pooja.chakrawarti@zysk.tech>
2023-12-20 14:05:35 +0530

    announcement notification fixed

--- app/Filament/Resources/AnnouncementResource/Pages/CreateAnnouncement.php
+++ app/Filament/Resources/AnnouncementResource/Pages/CreateAnnouncement.php
@@ -3,8 +3,9 @@
 namespace App\Filament\Resources\AnnouncementResource\Pages;
 
 use App\Filament\Resources\AnnouncementResource;
-use App\Models\Building\FlatTenant;
+use App\Models\Building\Building;
 use App\Models\ExpoPushNotification;
+use App\Models\User\User;
 use App\Traits\UtilsTrait;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Support\Facades\DB;
@@ -20,42 +21,48 @@ class CreateAnnouncement extends CreateRecord
 
     public function afterCreate()
     {
-        $users = FlatTenant::where('active', 1)
-            ->where('building_id', $this->data['building_id'])->first();
-        // if ($this->record->scheduled_at == now()) {
-            $expoPushTokens = ExpoPushNotification::where('user_id', $users->tenant_id)->pluck('token');
-            if ($expoPushTokens->count() > 0) {
-                foreach ($expoPushTokens as $expoPushToken) {
-                    $message = [
-                        'to' => $expoPushToken,
-                        'sound' => 'default',
-                        'url' => 'ComunityPostTab',
-                        'title' => 'New Announcement!',
-                        'body' => $this->record->content,
-                        'data' => ['notificationType' => 'ComunityPostTabNotice'],
-                    ];
-                    $this->expoNotification($message);
-                    DB::table('notifications')->insert([
-                        'id' => (string) \Ramsey\Uuid\Uuid::uuid4(),
-                        'type' => 'Filament\Notifications\DatabaseNotification',
-                        'notifiable_type' => 'App\Models\User\User',
-                        'notifiable_id' => $users->tenant_id,
-                        'data' => json_encode([
-                            'actions' => [],
-                            'body' => $this->record->content,
-                            'duration' => 'persistent',
-                            'icon' => 'heroicon-o-document-text',
-                            'iconColor' => 'warning',
+        if ($this->record->status == 'published') {
+            $building = Building::whereIn('id', $this->data['building_id'])->pluck('owner_association_id');
+            $allowedRoles = [1, 11];
+            $users = User::whereIn('owner_association_id', $building)->whereIn('role_id', $allowedRoles)->pluck('id');
+            // if ($this->record->scheduled_at == now()) {
+            foreach ($users as $user) {
+                $expoPushTokens = ExpoPushNotification::where('user_id', $user)->pluck('token');
+                if ($expoPushTokens->count() > 0) {
+                    foreach ($expoPushTokens as $expoPushToken) {
+                        $message = [
+                            'to' => $expoPushToken,
+                            'sound' => 'default',
+                            'url' => 'ComunityPostTab',
                             'title' => 'New Announcement!',
-                            'view' => 'notifications::notification',
-                            'viewData' => [],
-                            'format' => 'filament',
-                        ]),
-                        'created_at' => now()->format('Y-m-d H:i:s'),
-                        'updated_at' => now()->format('Y-m-d H:i:s'),
-                    ]);
+                            'body' => $this->record->content,
+                            'data' => ['notificationType' => 'ComunityPostTabNotice'],
+                        ];
+                        $this->expoNotification($message);
+                        DB::table('notifications')->insert([
+                            'id' => (string) \Ramsey\Uuid\Uuid::uuid4(),
+                            'type' => 'Filament\Notifications\DatabaseNotification',
+                            'notifiable_type' => 'App\Models\User\User',
+                            'notifiable_id' => $user,
+                            'data' => json_encode([
+                                'actions' => [],
+                                'body' => $this->record->content,
+                                'duration' => 'persistent',
+                                'icon' => 'heroicon-o-document-text',
+                                'iconColor' => 'warning',
+                                'title' => 'New Announcement!',
+                                'view' => 'notifications::notification',
+                                'viewData' => [],
+                                'format' => 'filament',
+                            ]),
+                            'created_at' => now()->format('Y-m-d H:i:s'),
+                            'updated_at' => now()->format('Y-m-d H:i:s'),
+                        ]);
+                    }
                 }
             }
-        // }
+
+            // }
+        }
     }
 }
