cc109ef920c4b39d419164ed5f249b4f7255354b
Prashanth <prashanth.c@zysk.tech>
2023-12-21 12:30:25 +0530

    FIX: Technician list in task tracker

--- app/Http/Controllers/Technician/TechnicianController.php
+++ app/Http/Controllers/Technician/TechnicianController.php
@@ -96,9 +96,9 @@ class TechnicianController extends Controller
     public function technicianList(Service $service, Vendor $vendor)
     {
         $contract = Contract::where('vendor_id', $vendor->id)
-            ->where('service_id', $service->id)->where('end_date', '>=', Carbon::now()->toDateString())->first()->service_id;
+            ->where('service_id', $service->id)->where('end_date', '>=', Carbon::now()->toDateString())->first()?->service_id;
         $serviceTechnician = DB::table('service_technician_vendor')->where('service_id', $contract)->pluck('technician_vendor_id');
-        $technicians = TechnicianVendor::whereIn('id', $serviceTechnician)->where('active', true)->get();
+        $technicians = TechnicianVendor::whereIn('id', $serviceTechnician)->where('active', true)->where('vendor_id',$vendor->id)->get();
         return ListTechnicianResource::collection($technicians);
     }
 
