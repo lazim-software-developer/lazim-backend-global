1157dbe8451312c7e9f311a3da5ba3e6ce1b783f
Prashanth <prashanth.c@zysk.tech>
2023-12-21 15:20:13 +0530

    UPDATE: Contract renewal Mail

---
+++ app/Jobs/ContractRenewalJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Snowfire\Beautymail\Beautymail;
+
+class ContractRenewalJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public $contract;
+    public $user;
+    public function __construct($contract, $user)
+    {
+        $this->contract = $contract;
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $beautymail = app()->make(Beautymail::class);
+        $beautymail->send('emails.contract_renewal_mail', ['user' => $this->user,'contract' => $this->contract], function($message) {
+            $message
+                ->to($this->user->email, $this->user->first_name)
+                ->subject('Welcome to Lazim!');
+        });
+    }
+}
