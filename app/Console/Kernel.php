ae32e40a5174ee1aacca1bfc5c91e32f1a1dc8bf
Prashanth <prashanth.c@zysk.tech>
2023-12-21 15:57:36 +0530

    FIX: Scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('telescope:prune')->daily();
         $schedule->command('app:deactivate-vendor')->daily();
         $schedule->command('budget:clean-imports')->daily();
-        $schedule->command('app:contract-renewal-mail')->everySecond();
+        $schedule->command('app:contract-renewal-mail')->daily();
     }
 
     /**
