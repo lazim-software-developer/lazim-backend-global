83e99795182cf218eeb3d6d4e98fb70ca35c77ac
Pooja <pooja.chakrawarti@zysk.tech>
2023-12-21 10:31:38 +0530

    removed unwanted code

--- app/Filament/Resources/ComplaintscomplaintResource/Pages/EditComplaintscomplaint.php
+++ app/Filament/Resources/ComplaintscomplaintResource/Pages/EditComplaintscomplaint.php
@@ -25,43 +25,6 @@ class EditComplaintscomplaint extends EditRecord
     public function afterSave()
     {
         $role = Role::where('id', auth()->user()->role_id)->first();
-
-        // //Assigned technician will notify
-        // if ($this->record->technician_id != null) {
-        //     $expoPushTokens = ExpoPushNotification::where('user_id', $this->record->technician_id)->pluck('token');
-        //     if ($expoPushTokens->count() > 0) {
-        //         foreach ($expoPushTokens as $expoPushToken) {
-        //             $message = [
-        //                 'to' => $expoPushToken,
-        //                 'sound' => 'default',
-        //                 'title' => 'New Complaint Assigned',
-        //                 'body' =>'A new complaint assigned to you.',
-        //                 'data' => ['notificationType' => 'app_notification'],
-        //             ];
-        //             $this->expoNotification($message);
-        //             DB::table('notifications')->insert([
-        //                 'id' => (string) \Ramsey\Uuid\Uuid::uuid4(),
-        //                 'type' => 'Filament\Notifications\DatabaseNotification',
-        //                 'notifiable_type' => 'App\Models\User\User',
-        //                 'notifiable_id' => $this->record->technician_id,
-        //                 'data' => json_encode([
-        //                     'actions' => [],
-        //                     'body' =>'A new complaint assigned to you.',
-        //                     'duration' => 'persistent',
-        //                     'icon' => 'heroicon-o-document-text',
-        //                     'iconColor' => 'warning',
-        //                     'title' => 'New Complaint Assigned',
-        //                     'view' => 'notifications::notification',
-        //                     'viewData' => [],
-        //                     'format' => 'filament'
-        //                 ]),
-        //                 'created_at' => now()->format('Y-m-d H:i:s'),
-        //                 'updated_at' => now()->format('Y-m-d H:i:s'),
-        //             ]);
-        //         }
-        //     }
-        // }
-
         //If complaint is closed by OA admin whoever raised complaint will notify
         if ($this->record->status == 'closed') {
             $expoPushTokens = ExpoPushNotification::where('user_id', $this->record->user_id)->pluck('token');
