de836d4965f79e0f6e43f13205e2a49558035f5a
Subhshreddy <subhash.reddy@zysk.tech>
2023-12-29 19:02:05 +0530

    separate resource to contracts and showing budget amount.

---
+++ app/Filament/Resources/ContractResource/Pages/ListContracts.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\ContractResource\Pages;
+
+use Filament\Actions;
+use App\Models\Building\Building;
+use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ContractResource;
+
+class ListContracts extends ListRecords
+{
+    protected static string $resource = ContractResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            // Actions\CreateAction::make(),
+        ];
+    }
+    protected function getTableQuery(): Builder
+    {
+        return parent::getTableQuery()->whereIn('building_id',Building::where('owner_association_id',auth()->user()->owner_association_id)->pluck('id'));
+    }
+}
