6526a515311305b888e3191d9df2388220710404
Prashanth <prashanth.c@zysk.tech>
2024-01-02 12:31:20 +0530

    FIX: building filters

--- app/Http/Controllers/Vendor/VendorBuildingController.php
+++ app/Http/Controllers/Vendor/VendorBuildingController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers\Vendor;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Building\BuildingResource;
 use App\Models\Vendor\Vendor;
+use Illuminate\Http\Request;
 
 class VendorBuildingController extends Controller
 {
-    public function listBuildings(Vendor $vendor){
+    public function listBuildings(Request $request,Vendor $vendor){
 
-        
         $buildings = $vendor->buildings->where('pivot.active', true)->where('pivot.end_date','>',now()->toDateString())->unique();
+
+        if ($request->has('filter') && $request->input('filter') == true) {
+            $buildings = $vendor->buildings->unique();
+        }
 
         return BuildingResource::collection($buildings);
     }
