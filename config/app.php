58ca8248984c120049e8477e1a4c03d8bc477da8
Prashanth <prashanth.c@zysk.tech>
2024-01-03 11:15:10 +0530

    UPDATE: Time Zone

--- config/app.php
+++ config/app.php
@@ -71,7 +71,7 @@ return [
     |
     */
 
-    'timezone' => 'Asia/Kolkata',
+    'timezone' => env('TIME_ZONE','Asia/Dubai'),
 
     /*
     |--------------------------------------------------------------------------
