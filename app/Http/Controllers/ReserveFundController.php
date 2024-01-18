61be3474522db94fb221332aef6d74a1a305d1c1
Prashanth <prashanth.c@zysk.tech>
2024-01-18 12:02:04 +0530

    UPDATE: Reserve fund- import,statement,controller, blade file,

--- app/Http/Controllers/ReserveFundController.php
+++ app/Http/Controllers/ReserveFundController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Accounting\Invoice;
 use App\Models\Accounting\OAMReceipts;
+use App\Models\GeneralFund;
 use Illuminate\Http\Request;
 
 class ReserveFundController extends Controller
@@ -12,17 +13,11 @@ class ReserveFundController extends Controller
     {
         $year = $request->input('year');
         $buildingId = $request->input('building_id');
+                                                                
+        $reserves = GeneralFund::where('type','Reserve Fund')->where('credited_amount','>',0)->where('building_id',$buildingId)->whereYear('statement_date', $year)->paginate();
+        $expenses = GeneralFund::where('type','Reserve Fund')->where('debited_amount','>',0)->where('building_id',$buildingId)->whereYear('statement_date', $year)->paginate();
+        
 
-        $receipts= OAMReceipts::where(['building_id'=>$buildingId,'payment_mode'=>'Noqodi Payment'])->whereYear('receipt_date', $year)->get()
-                                                                ->sum(function ($receipt) {
-                                                                    $noqodiInfo = json_decode($receipt->noqodi_info, true);
-                                                                    return $noqodiInfo['reservedFundAmount'] ?? 0;
-                                                                });
-
-        // $expenses = Invoice::where(['building_id' => $buildingId,'status' => 'approved'])->whereNotNull('payment')->whereYear('date', $year)->get();
-
-        // $totalExpense = number_format($expenses?->sum('payment'),2);
-
-        return view('partials.reserve-fund-statement', ['receipt' => $receipts]);
+        return view('partials.reserve-fund-statement', ['reserves' => $reserves, 'expenses' => $expenses]);
     }
 }
