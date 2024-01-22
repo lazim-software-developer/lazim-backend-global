d41f2143d7ad9a2ebbaca4ed903dcb1c00a48a66
Prashanth <prashanth.c@zysk.tech>
2024-01-22 14:45:01 +0530

    FIX: cooling account wrong file upload error handle

--- app/Imports/CoolingAccountImport.php
+++ app/Imports/CoolingAccountImport.php
@@ -32,42 +32,65 @@ class CoolingAccountImport implements ToCollection, WithHeadingRow
     public function collection(Collection $rows)
     {
         $date = Carbon::parse($this->month)->format('Y-m-d');
+        // Define the expected headings
+        $expectedHeadings = [
+            'unit_no','opening_balance_receivable_advance',
+            'in_unit_consumption', 'in_unit_demand_charge',
+            'in_unit_security_deposit', 'in_unit_billing_charges',
+            'in_unit_other_charges', 'receipts', 'closing_balance'
+           ];
+   
+           // Extract the headings from the first row
+           $extractedHeadings = array_keys($rows->first()->toArray());
+   
+           // Check if all expected headings are present in the extracted headings
+           $missingHeadings = array_diff($expectedHeadings, $extractedHeadings);
+   
+           if (!empty($missingHeadings)) {
+               Notification::make()
+                   ->title("Upload valid excel file.")
+                   ->danger()
+                   ->body("Missing headings: " . implode(', ', $missingHeadings))
+                   ->send();
+               return 'failure';
+           } else {
 
-        foreach ($rows as $row) {
-            $flatId = Flat::where(['building_id' => $this->buildingId, 'property_number' => $row['unit_no']])->first()?->id;
-            if(! $flatId) {
-                continue;
-            }
-            if (CoolingAccount::where(['building_id' => $this->buildingId, 'flat_id' => $flatId])->exists()) {
-                Notification::make()
-                    ->title("You have already uploaded details for the month ".Str::ucfirst($this->month))
-                    ->danger()
-                    ->send();
-                return 'error';
-            }
+            foreach ($rows as $row) {
+                $flatId = Flat::where(['building_id' => $this->buildingId, 'property_number' => $row['unit_no']])->first()?->id;
+                if(! $flatId) {
+                    continue;
+                }
+                if (CoolingAccount::where(['building_id' => $this->buildingId, 'flat_id' => $flatId])->exists()) {
+                    Notification::make()
+                        ->title("You have already uploaded details for the month ".Str::ucfirst($this->month))
+                        ->danger()
+                        ->send();
+                    return 'error';
+                }
 
-            CoolingAccount::firstOrCreate(
-                [
-                    'building_id'           => $this->buildingId,
-                    'flat_id'               => $row['unit_no'],
-                    'date'                  => $date,
-                ],
-                [
-                    'opening_balance'       => $row['opening_balance_receivable_advance'],
-                    'consumption'           => $row['in_unit_consumption'],
-                    'demand_charge'         => $row['in_unit_demand_charge'],
-                    'security_deposit'      => $row['in_unit_security_deposit'],
-                    'billing_charges'       => $row['in_unit_billing_charges'],
-                    'other_charges'         => $row['in_unit_other_charges'],
-                    'receipts'              => $row['receipts'],
-                    'closing_balance'       => $row['closing_balance'],
-                ]
-            );
+                CoolingAccount::firstOrCreate(
+                    [
+                        'building_id'           => $this->buildingId,
+                        'flat_id'               => $row['unit_no'],
+                        'date'                  => $date,
+                    ],
+                    [
+                        'opening_balance'       => $row['opening_balance_receivable_advance'],
+                        'consumption'           => $row['in_unit_consumption'],
+                        'demand_charge'         => $row['in_unit_demand_charge'],
+                        'security_deposit'      => $row['in_unit_security_deposit'],
+                        'billing_charges'       => $row['in_unit_billing_charges'],
+                        'other_charges'         => $row['in_unit_other_charges'],
+                        'receipts'              => $row['receipts'],
+                        'closing_balance'       => $row['closing_balance'],
+                    ]
+                );
+            }
+            Notification::make()
+                ->title("Details uploaded successfully")
+                ->success()
+                ->send();
+            return 'success';
         }
-        Notification::make()
-            ->title("Details uploaded successfully")
-            ->success()
-            ->send();
-        return 'success';
     }
 }
