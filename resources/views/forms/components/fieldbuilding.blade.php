830bae5421bfa625b4cf6b944dfcda58f18f7af4
Prashanth <prashanth.c@zysk.tech>
2024-01-23 11:19:49 +0530

    FIX: Guest Registration Form

--- resources/views/forms/components/fieldbuilding.blade.php
+++ resources/views/forms/components/fieldbuilding.blade.php
@@ -5,7 +5,12 @@
     <div x-data="{ state: $wire.entangle('{{ $getStatePath() }}') }">
         <!-- Interact with the `state` property in Alpine.js -->
         <div style="border: 1.5px lightgray solid;padding:7px;border-radius: 8px;">
-            {{ $getRecord()->flatVisitor->building->name}}
+            @php
+            $images = $getRecord()->flatVisitor->guestDocuments->where('documentable_type','App\Models\Visitor\FlatVisitor')->pluck('url');
+            @endphp
+            @foreach($images as $image)
+                <a href="https://lazim-dev.s3.ap-south-1.amazonaws.com/{{$image}}" style="color:blue;">View Passport</a><br>
+            @endforeach
         </div>
     </div>
 </x-dynamic-component>
