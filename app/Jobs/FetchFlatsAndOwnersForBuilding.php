97d9b1b0ddc8a16c941407e5dd16a6212f982252
shilpaGowda <shilpa@zysk.tech>
2024-01-24 16:12:40 +0530

    fix: test flats API

--- app/Jobs/FetchFlatsAndOwnersForBuilding.php
+++ app/Jobs/FetchFlatsAndOwnersForBuilding.php
@@ -31,32 +31,34 @@ class FetchFlatsAndOwnersForBuilding implements ShouldQueue
         ])->get(env("MOLLAK_API_URL") . "/sync/propertygroups/" . $this->building->property_group_id . "/units");
 
         $data = $response->json();
+        
+        Log::info("Flats executed", [$data]);
 
-        if ($data['response'] != null) {
-            foreach ($data['response']['units'] as $property) {
-                Log::info("Data", [$property]);
-                $flat = Flat::updateOrCreate(
-                    [
-                        'property_number' => $property['unitNumber'],
-                        'mollak_property_id' => $property['mollakPropertyId'],
-                        'building_id' => $this->building->id,
-                        'owner_association_id' => $this->building->owner_association_id,
-                    ],
-                    [
-                        'plot_number' => $property['plotNumber'],
-                        'suit_area' => $property['suitArea'],
-                        'actual_area' => $property['actualArea'],
-                        'balcony_area' => $property['balconyArea'],
-                        'applicable_area' => $property['applicableArea'],
-                        'virtual_account_number' => $property['virtualAccountNumber'],
-                        'parking_count' => $property['parkingCount'],
-                        'property_type' => 'NA'
-                    ]
-                );
+        // if ($data['response'] != null) {
+        //     foreach ($data['response']['units'] as $property) {
+        //         Log::info("Data", [$property]);
+        //         $flat = Flat::updateOrCreate(
+        //             [
+        //                 'property_number' => $property['unitNumber'],
+        //                 'mollak_property_id' => $property['mollakPropertyId'],
+        //                 'building_id' => $this->building->id,
+        //                 'owner_association_id' => $this->building->owner_association_id,
+        //             ],
+        //             [
+        //                 'plot_number' => $property['plotNumber'],
+        //                 'suit_area' => $property['suitArea'],
+        //                 'actual_area' => $property['actualArea'],
+        //                 'balcony_area' => $property['balconyArea'],
+        //                 'applicable_area' => $property['applicableArea'],
+        //                 'virtual_account_number' => $property['virtualAccountNumber'],
+        //                 'parking_count' => $property['parkingCount'],
+        //                 'property_type' => 'NA'
+        //             ]
+        //         );
 
-                // Dispatch job to fetch owners for the flat
-                FetchOwnersForFlat::dispatch($flat);
-            }
-        }
+        //         // Dispatch job to fetch owners for the flat
+        //         FetchOwnersForFlat::dispatch($flat);
+        //     }
+        // }
     }
 }
