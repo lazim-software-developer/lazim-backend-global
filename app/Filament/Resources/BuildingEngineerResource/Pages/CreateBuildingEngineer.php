e44f6734d61e90c1cd1c564e6a89889ff59c9ce0
Subhshreddy <subhash.reddy@zysk.tech>
2024-01-25 18:56:40 +0530

    creation of Building Engineers for oa and md.

---
+++ app/Filament/Resources/BuildingEngineerResource/Pages/CreateBuildingEngineer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Resources\BuildingEngineerResource\Pages;
+
+use Filament\Actions;
+use App\Models\Master\Role;
+use Illuminate\Support\Str;
+use App\Jobs\AccountsManagerJob;
+use Illuminate\Support\Facades\Hash;
+use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\BuildingEngineerResource;
+
+class CreateBuildingEngineer extends CreateRecord
+{
+    protected static string $resource = BuildingEngineerResource::class;
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        
+        $data['phone'] = '971'.$data['phone'];
+        $data['email_verified'] = true;
+        $data['phone_verified'] = true;
+        $data['role_id'] = Role::where('name', 'Building Engineer')->first()->id;
+        $data['owner_association_id'] = auth()->user()->owner_association_id;
+
+        return $data;
+    }
+    protected function afterCreate(): void
+    {
+        $user = $this->record;
+        $password = Str::random(12);
+        $user->password = Hash::make($password);
+        $user->save();
+        AccountsManagerJob::dispatch($user, $password);
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
