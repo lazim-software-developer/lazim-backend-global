c72ad57e68277287fe7280cdd905ee9bc86813fe
Prashanth <prashanth.c@zysk.tech>
2024-01-31 19:39:56 +0530

    FIX: Snags Notification

--- app/Filament/Resources/SnagsResource/Pages/EditSnags.php
+++ app/Filament/Resources/SnagsResource/Pages/EditSnags.php
@@ -59,6 +59,40 @@ class EditSnags extends EditRecord
                     ]);
                 }
             }
+            if($this->record->technician_id){
+                $expoPushTokens = ExpoPushNotification::where('user_id', $this->record->technician_id)->pluck('token');
+                if ($expoPushTokens->count() > 0) {
+                    foreach ($expoPushTokens as $expoPushToken) {
+                        $message = [
+                            'to' => $expoPushToken,
+                            'sound' => 'default',
+                            'title' => 'Snags status',
+                            'body' => 'A Snag has been resolved by '.$role->name.' : '.auth()->user()->first_name,
+                            'data' => ['notificationType' => 'ResolvedRequests'],
+                        ];
+                        $this->expoNotification($message);
+                        DB::table('notifications')->insert([
+                            'id' => (string) \Ramsey\Uuid\Uuid::uuid4(),
+                            'type' => 'Filament\Notifications\DatabaseNotification',
+                            'notifiable_type' => 'App\Models\User\User',
+                            'notifiable_id' => $this->record->technician_id,
+                            'data' => json_encode([
+                                'actions' => [],
+                                'body' => 'A Snag has been resolved by '.$role->name.' : '.auth()->user()->first_name,
+                                'duration' => 'persistent',
+                                'icon' => 'heroicon-o-document-text',
+                                'iconColor' => 'warning',
+                                'title' => 'Snags status',
+                                'view' => 'notifications::notification',
+                                'viewData' => [],
+                                'format' => 'filament'
+                            ]),
+                            'created_at' => now()->format('Y-m-d H:i:s'),
+                            'updated_at' => now()->format('Y-m-d H:i:s'),
+                        ]);
+                    }
+                }
+            }
         }
     }
 }
