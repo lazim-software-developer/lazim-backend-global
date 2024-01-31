d8c895f091e019ddbd2d84ba2434764f453fcf33
Prashanth <prashanth.c@zysk.tech>
2024-01-31 19:31:35 +0530

    UPDATE: Snag closed notification

--- app/Filament/Resources/SnagsResource/Pages/EditSnags.php
+++ app/Filament/Resources/SnagsResource/Pages/EditSnags.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\SnagsResource\Pages;
 
 use App\Filament\Resources\SnagsResource;
+use App\Models\ExpoPushNotification;
+use App\Models\Master\Role;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\DB;
 
 class EditSnags extends EditRecord
 {
@@ -15,5 +18,45 @@ class EditSnags extends EditRecord
         return [
             // Actions\DeleteAction::make(),
         ];
+    }
+
+    public function afterSave()
+    {
+        $role = Role::where('id', auth()->user()->role_id)->first();
+        //If complaint is closed by OA admin whoever raised complaint will notify
+        if ($this->record->status == 'closed') {
+            $expoPushTokens = ExpoPushNotification::where('user_id', $this->record->user_id)->pluck('token');
+            if ($expoPushTokens->count() > 0) {
+                foreach ($expoPushTokens as $expoPushToken) {
+                    $message = [
+                        'to' => $expoPushToken,
+                        'sound' => 'default',
+                        'title' => 'Snags status',
+                        'body' => 'Your Snag has been resolved by '.$role->name.' : '.auth()->user()->first_name,
+                        'data' => ['notificationType' => 'MyComplaints'],
+                    ];
+                    $this->expoNotification($message);
+                    DB::table('notifications')->insert([
+                        'id' => (string) \Ramsey\Uuid\Uuid::uuid4(),
+                        'type' => 'Filament\Notifications\DatabaseNotification',
+                        'notifiable_type' => 'App\Models\User\User',
+                        'notifiable_id' => $this->record->user_id,
+                        'data' => json_encode([
+                            'actions' => [],
+                            'body' => 'Your Snag has been resolved by '.$role->name.' : '.auth()->user()->first_name,
+                            'duration' => 'persistent',
+                            'icon' => 'heroicon-o-document-text',
+                            'iconColor' => 'warning',
+                            'title' => 'Snags status',
+                            'view' => 'notifications::notification',
+                            'viewData' => [],
+                            'format' => 'filament'
+                        ]),
+                        'created_at' => now()->format('Y-m-d H:i:s'),
+                        'updated_at' => now()->format('Y-m-d H:i:s'),
+                    ]);
+                }
+            }
+        }
     }
 }
