68fa6e39b12f83ecc3658d1de7d64d0c6895cec0
Subhshreddy <subhash.reddy@zysk.tech>
2024-02-07 10:57:13 +0530

    Resident approval.

--- app/Models/UserApproval.php
+++ app/Models/UserApproval.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserApproval extends Model
 {
     use HasFactory;
 
     protected $fillable = ['user_id', 'document_type', 'status', 'remarks', 'document', 'updated_by'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
