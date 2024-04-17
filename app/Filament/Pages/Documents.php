909becefe8b54d1bacb9f450da04e40672007236
Prashanth <prashanth.c@zysk.tech>
2024-04-17 21:33:40 +0530

    HOTFIX: Rera

--- app/Filament/Pages/Documents.php
+++ app/Filament/Pages/Documents.php
@@ -74,12 +74,12 @@ class Documents extends Page implements HasTable
         //     ['id' => 453522143, 'name' => '1-Jan-2022 To 31-Dec-2022', 'from' => '2022-01-01T20:00:00', 'to' => '2022-12-31T20:00:00'],
         //     ['id' => 604334600, 'name' => '1-Jan-2023 To 31-Dec-2023', 'from' => '2023-01-01T20:00:00', 'to' => '2023-12-31T20:00:00']
         // ];
-            $datas = PropertyGroupResource::collection($data['response']['propertyGroups']);
-            foreach ($datas as $data){
+            // $datas = PropertyGroupResource::collection($data['response']['propertyGroups']);
+            // foreach ($datas as $data){
                 
-                dd($data);
-                Log::info(json_decode($data));
-            }
+            //     dd($data);
+            //     Log::info(json_decode($data));
+            // }
         return [
             'services' => ServiceParameterResource::collection(ServiceParameter::all()),
             'propertyGroups' => PropertyGroupResource::collection($data['response']['propertyGroups']),
--- app/Http/Resources/Master/PropertyGroupResource.php
+++ app/Http/Resources/Master/PropertyGroupResource.php
@@ -12,25 +12,12 @@ class PropertyGroupResource extends JsonResource
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
-    // public function toArray($request)
-    // {
-    //     return [
-    //         'id' => $this['propertyGroupId'],
-    //         'name' => $this['propertyGroupName']['englishName'],
-    //     ];
-    // }
-
     public function toArray($request)
     {
         return [
-            'id' => $this->resource['propertyGroupId'],
-            'name' => $this->getName(), // Assuming you've added a method `getName`
+            'id' => $this['propertyGroupId'],
+            'name' => $this['propertyGroupName']['englishName'],
         ];
-    }
-
-    public function getName()
-    {
-        return $this->resource['propertyGroupName']['englishName'] ?? 'Default Name'; 
     }
 }
 
