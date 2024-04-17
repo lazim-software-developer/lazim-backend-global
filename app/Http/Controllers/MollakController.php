18ec60f0f3efa9f5ba502e68eec8a60f169f09c2
Prashanth <prashanth.c@zysk.tech>
2024-04-17 14:51:06 +0530

    Testing mollak api

--- app/Http/Controllers/MollakController.php
+++ app/Http/Controllers/MollakController.php
@@ -109,9 +109,9 @@ class MollakController extends Controller
         //     ],
         // ]);
 
-        LOG::info("MOLLA ". $response);
+        // LOG::info("MOLLA ". $response);
 
-        return $data = $response->json();
+        // return $data = $response->json();
     }
 
     public function sendSMS(Request $request)
