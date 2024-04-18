ddc5676fc79d20296dd7c550e515455817f5c4bb
Prashanth <prashanth.c@zysk.tech>
2024-04-18 16:09:36 +0530

    HOTFIX: Rera

--- resources/views/filament/pages/documents.blade.php
+++ resources/views/filament/pages/documents.blade.php
@@ -70,7 +70,7 @@
                     <div class="mb-3" style="display: flex; align-items: center; justify-content: space-between; margin-bottom: 10px;">
                         <label style="flex-basis: 40%; margin-right: 10px;">{{ $service->name }}*</label>
                         <div style="flex-basis: 58%;">
-                            <input type="file" id="file_{{ $service->value }}" name="{{$service->value}}" class="form-control" style="display: block; width: 100%;" required>
+                            <input type="file" id="file_{{ $service->value }}" name="{{$service->value}}" class="form-control" style="display: block; width: 100%;" >
                         </div>
                     </div>
                     @endforeach
