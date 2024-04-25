66a7fab67af8f1a7121113bdf819ae6a85a7457c
Prashanth <prashanth.c@zysk.tech>
2024-04-25 18:02:34 +0530

    UPDATE: column field alignment

--- app/Filament/Resources/LedgersResource.php
+++ app/Filament/Resources/LedgersResource.php
@@ -64,15 +64,18 @@ class LedgersResource extends Resource
                     ->limit(20)
                     ->label('Invoice'),
                 TextColumn::make('invoice_amount')->formatStateUsing(fn ($state) => number_format($state, 2))
-                    ->label('Invoice amount'),
-                ViewColumn::make('Paid Amount')->view('tables.columns.invoice-amount-paid'),
+                    ->label('Invoice amount')->alignEnd(),
+                ViewColumn::make('Paid Amount')->view('tables.columns.invoice-amount-paid')
+                    ->alignEnd(),
                 TextColumn::make('due_amount')
                     ->searchable()
                     ->default("NA")
+                    ->alignEnd()
                     ->label('Due Balance'),
                 TextColumn::make('previous_balance')
                     ->searchable()
                     ->default("NA")
+                    ->alignEnd()
                     ->label('Previous Balance'),
             ])
             ->defaultSort('created_at', 'desc')
