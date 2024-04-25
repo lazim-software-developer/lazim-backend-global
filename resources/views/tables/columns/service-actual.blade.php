96ae0c65841a88f21ec5ff9a087911632c20136b
Prashanth <prashanth.c@zysk.tech>
2024-04-25 15:05:38 +0530

    UPDATE: Budget vs Actual logic

--- resources/views/tables/columns/service-actual.blade.php
+++ resources/views/tables/columns/service-actual.blade.php
@@ -2,17 +2,19 @@
     @php
         use App\Models\Vendor\Contract;
         use App\Models\Accounting\Budgetitem;
+        use App\Models\Accounting\Invoice;
 
         $serviceId = $getRecord()->service_id;
         $buildingId = $getRecord()->budget->building_id;
 
-        $amount = Contract::where('service_id',$serviceId)->where('building_id',$buildingId)->first()?->amount  ;
-        if($amount){
-            $amount = number_format($amount,2);
+        $contractId = Contract::where('service_id',$serviceId)->where('building_id',$buildingId)->where('end_date','>=',now()->toDateString())->first()?->id ;
+        $invoiceAmount =  Invoice::where('contract_id',$contractId)->where('building_id',$buildingId)->sum('invoice_amount');
+        if($invoiceAmount){
+            $invoiceAmount = number_format($invoiceAmount,2);
         }
         else{
-            $amount = 'N/A';
+            $invoiceAmount = 'N/A';
         }
     @endphp
-    {{ $amount }}
+    {{ $invoiceAmount }}
 </div>
--- resources/views/tables/columns/service-surplus.blade.php
+++ resources/views/tables/columns/service-surplus.blade.php
@@ -6,9 +6,10 @@
         $serviceId = $getRecord()->service_id;
         $buildingId = $getRecord()->budget->building_id;
         $budget = $getRecord()->budget_excl_vat;
-        $amount = Contract::where('service_id',$serviceId)->where('building_id',$buildingId)->first()?->amount;
-        if ($amount){
-            $balance = number_format($budget - $amount,2);
+        $contractId = Contract::where('service_id',$serviceId)->where('building_id',$buildingId)->where('end_date','>=',now()->toDateString())->first()?->id ;
+        $invoiceAmount =  Invoice::where('contract_id',$contractId)->where('building_id',$buildingId)->sum('invoice_amount');
+        if ($invoiceAmount){
+            $balance = number_format($budget - $invoiceAmount,2);
         }
         else{
             $balance = '---';
