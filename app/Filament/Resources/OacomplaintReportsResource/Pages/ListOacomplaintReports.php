095dc0fa88fe94be909b9c665e717d4eb5624c77
Subhshreddy <subhash.reddy@zysk.tech>
2024-05-20 12:38:28 +0530

    implemented new change requests.

--- app/Filament/Resources/OacomplaintReportsResource/Pages/ListOacomplaintReports.php
+++ app/Filament/Resources/OacomplaintReportsResource/Pages/ListOacomplaintReports.php
@@ -3,12 +3,24 @@
 namespace App\Filament\Resources\OacomplaintReportsResource\Pages;
 
 use App\Filament\Resources\OacomplaintReportsResource;
+use App\Models\Building\Building;
+use App\Models\Master\Role;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListOacomplaintReports extends ListRecords
 {
     protected static string $resource = OacomplaintReportsResource::class;
+    protected function getTableQuery(): Builder
+    {
+        $buildings = Building::all()->where('owner_association_id', auth()->user()->owner_association_id)->pluck('id')->toArray();
+
+        if (Role::where('id', auth()->user()->role_id)->first()->name != 'Admin') {
+            return parent::getTableQuery()->whereIn('building_id', $buildings);
+        }
+        return parent::getTableQuery();
+    }
 
     protected function getHeaderActions(): array
     {
@@ -16,5 +28,5 @@ class ListOacomplaintReports extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
-    
+
 }
