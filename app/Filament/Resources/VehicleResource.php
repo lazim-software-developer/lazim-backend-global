0b599467d87b1d84bbc951e352bfab301cdad608
Prashanth <prashanth.c@zysk.tech>
2024-05-21 13:00:52 +0530

    FEAT: Vehicles management

---
+++ app/Filament/Resources/VehicleResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\VehicleResource\Pages;
+use App\Filament\Resources\VehicleResource\RelationManagers;
+use App\Models\Master\Role;
+use App\Models\User\User;
+use App\Models\Vehicle;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class VehicleResource extends Resource
+{
+    protected static ?string $model = Vehicle::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('vehicle_number')->alphaNum()->unique(),
+                TextInput::make('makani_number')->alphaNum()->unique(),
+                Select::make('user_id')->label('Owner')->searchable()->preload()
+                ->options(function(){
+                    return User::where('owner_association_id', auth()->user()->owner_association_id)->whereIn('role_id',Role::whereIn('name',['Tenant','Owner'])->pluck('id'))->pluck('first_name','id');
+                })
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('vehicle_number'),
+                TextColumn::make('makani_number'),
+                TextColumn::make('user.first_name')->label('Owner'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                // Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    // Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVehicles::route('/'),
+            'create' => Pages\CreateVehicle::route('/create'),
+            'view' => Pages\ViewVehicle::route('/{record}'),
+            'edit' => Pages\EditVehicle::route('/{record}/edit'),
+        ];
+    }    
+}
