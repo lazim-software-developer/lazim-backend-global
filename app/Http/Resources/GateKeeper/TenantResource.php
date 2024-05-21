cd2d56fd933835df5aa303094c2d2fd878749869
Prashanth <prashanth.c@zysk.tech>
2024-05-21 17:11:54 +0530

    UPDATE: Resident directory

--- app/Http/Resources/GateKeeper/TenantResource.php
+++ app/Http/Resources/GateKeeper/TenantResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\GateKeeper;
 
+use App\Http\Resources\FamilyMembersResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Storage;
@@ -20,7 +21,10 @@ class TenantResource extends JsonResource
             'flat' => $this->flat->property_number,
             'flat_id' => $this->flat_id,
             'user_name' => $this->user->first_name,
-            'profile_pic' => $this->user->profile_photo ? Storage::disk('s3')->url($this->user->profile_photo) : null
+            'user_email' => $this->user->email,
+            'user_phone' => $this->user->phone,
+            'profile_pic' => $this->user->profile_photo ? Storage::disk('s3')->url($this->user->profile_photo) : null,
+            'family_members' => FamilyMembersResource::collection($this->user->residentialForm()->where('building_id', $this->building_id)->where('status', 'approved'))
         ];
     }
 }
