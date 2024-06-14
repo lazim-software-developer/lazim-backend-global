67dbf745a8249f5ed23f5547d4f2287657babea3
Prashanth <prashanth.c@zysk.tech>
2024-06-14 12:26:59 +0530

    FIX: handle oa related data item resource

--- app/Filament/Resources/ItemResource.php
+++ app/Filament/Resources/ItemResource.php
@@ -79,7 +79,8 @@ class ItemResource extends Resource
 
     public static function table(Table $table): Table
     {
-        return $table
+        $buildings = Building::where('owner_association_id',auth()->user()->owner_association_id)->pluck('id');
+        return $table->modifyQueryUsing(fn(Builder $query) => $query->whereIn('building_id', $buildings)->orderBy('created_at','desc')->withoutGlobalScopes())
             ->columns([
                 TextColumn::make('name')
                     ->searchable(),
