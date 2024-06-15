b04ecfe2d90301abafeed8db2ecfc676b0ac69d0
Prashanth <prashanth.c@zysk.tech>
2024-06-15 19:04:07 +0530

    FIX: flatpickr

--- app/Filament/Resources/VendorLedgersResource.php
+++ app/Filament/Resources/VendorLedgersResource.php
@@ -114,8 +114,8 @@ class VendorLedgersResource extends Resource
             ->filters([
                 Filter::make('date')
                     ->form([
-                        Flatpickr::make('Date')
-                            ->range(true),
+                        // Flatpickr::make('Date')
+                        //     ->range(true),
                     ])
                     ->query(function (Builder $query, array $data): Builder {
                         if (isset($data['Date'])) {
