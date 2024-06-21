4b95e47480886c05e0e01bc16d5e3fe5c1d389be
Lokendra <info@ilaj.ae>
2024-06-21 18:05:48 +0400

    Working on request form ticket generation

---
+++ resources/views/emails/send_moveinout-blade.php
@@ -0,0 +1,94 @@
+@extends('beautymail::templates.minty')
+
+@section('content')
+
+@include('beautymail::templates.minty.contentStart')
+<tr>
+	<td class="paragraph">
+		Dear {{$user->first_name}},
+	</td>
+</tr>
+<tr>
+	<td width="100%" height="20"></td>
+</tr>
+<tr>
+	<td class="paragraph">
+		We are pleased to inform you that your move-in/move-out request has been successfully submitted. Below are your ticket details:
+	</td>
+</tr>
+<tr>
+	<td width="100%" height="25"></td>
+</tr>
+<tr>
+	<td class="title">
+		Ticket Details:
+	</td>
+</tr>
+<tr>
+	<td width="100%" height="10"></td>
+</tr>
+<tr>
+	<td class="paragraph">
+		<strong>Ticket Number: </strong> {{$ticket_number}}
+	</td>
+</tr>
+<tr>
+	<td class="paragraph">
+		<strong>Building ID: </strong> {{$building_id}}
+	</td>
+</tr>
+<tr>
+	<td class="paragraph">
+		<strong>Flat ID: </strong> {{$flat_id}}
+	</td>
+</tr>
+<tr>
+	<td class="paragraph">
+		<strong>Type: </strong> {{$type}}
+	</td>
+</tr>
+<tr>
+	<td class="paragraph">
+		<strong>Moving Date: </strong> {{$moving_date}}
+	</td>
+</tr>
+<tr>
+	<td class="paragraph">
+		<strong>Moving Time: </strong> {{$moving_time}}
+	</td>
+</tr>
+<tr>
+	<td class="paragraph">
+		<strong>Time Preference: </strong> {{$time_preference}}
+	</td>
+</tr>
+<tr>
+	<td width="100%" height="25"></td>
+</tr>
+<tr>
+	<td class="paragraph">
+		Thank you for choosing Lazim. We are confident that you will find great value in our platform, and we look forward to serving you.
+	</td>
+</tr>
+<tr>
+	<td width="100%" height="25"></td>
+</tr>
+<tr>
+	<td class="paragraph">
+		Warm regards,
+	</td>
+</tr>
+<tr>
+	<td width="100%" height="5"></td>
+</tr>
+<tr>
+	<td class="paragraph">
+		The Lazim Team
+	</td>
+</tr>
+<tr>
+	<td width="100%" height="25"></td>
+</tr>
+@include('beautymail::templates.minty.contentEnd')
+
+@stop
