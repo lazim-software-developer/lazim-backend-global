faadad660fccf1c41fa08f537ac294d7b784255a
Prashanth <prashanth.c@zysk.tech>
2024-07-02 14:43:00 +0530

    UPDATE: Restrict tenant without active contract

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -117,8 +117,7 @@ class AuthController extends Controller
             ]);
         }
         if ($user && $user?->role->name == 'Tenant' ){
-            $user->residences->where('active',true)->count() < 1;
-            abort_if(true, 422, "Currently,you don't have any active contract" );
+            abort_if($user->residences->where('active',true)->count() < 1, 422, "Currently, you don't have any active contract" );
         }
 
         // Check if the user's email and phone number is verified
