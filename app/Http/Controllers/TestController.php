db0bfde0d852103957f359c26ddaf8925d8b161a
Prashanth <prashanth.c@zysk.tech>
2024-07-12 19:53:50 +0530

    TEST: RERA

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -301,11 +301,11 @@ class TestController extends Controller
             'amount'            => 987.2,
             'utility_name'      => 'Electricity Consumption',
             'provider_name'     => 'Dubai Electricity and Water Authority (DEWA)',
-            'trend'             => [
-                'duration'     => '2021-07',
-                'duration_str' => 'Jul 2021',
-                'amount'       => 23455.0,
-            ],
+            'trend'             => []
+            //     'duration'     => '2021-07',
+            //     'duration_str' => 'Jul 2021',
+            //     'amount'       => 23455.0,
+            // ],
         ];
         $data->BudgetVsActual  = $budget_vs_actual;
         $data->GeneralFund     = $general_fund_statement;
