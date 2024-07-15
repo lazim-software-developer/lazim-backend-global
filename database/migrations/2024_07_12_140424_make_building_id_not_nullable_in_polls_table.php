e4b4b48da332f13de3257df7f55a5fdfc35d384c
Prashanth <prashanth.c@zysk.tech>
2024-07-15 11:31:50 +0530

    FIX: migration

--- database/migrations/2024_07_12_140424_make_building_id_not_nullable_in_polls_table.php
+++ database/migrations/2024_07_12_140424_make_building_id_not_nullable_in_polls_table.php
@@ -12,7 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('polls', function (Blueprint $table) {
+            // Drop the foreign key constraint
+            $table->dropForeign(['building_id']);
+    
+            // Change the column
             $table->bigInteger('building_id')->nullable()->change();
+    
+            // Re-add the foreign key constraint if necessary
+            $table->foreign('building_id')->references('id')->on('buildings');
         });
     }
 
@@ -22,7 +29,11 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('polls', function (Blueprint $table) {
+            $table->dropForeign(['building_id']);
+    
             $table->unsignedBigInteger('building_id')->nullable(false)->change();
+    
+            $table->foreign('building_id')->references('id')->on('buildings');
         });
     }
 };
