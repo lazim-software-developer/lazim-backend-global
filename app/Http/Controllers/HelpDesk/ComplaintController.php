b5048b8e9baa57596e9b22ca0dffcc417237dd1b
Subhshreddy <subhash.reddy@zysk.tech>
2024-07-16 20:11:17 +0530

    refactor: Update flatTenant retrieval logic in ComplaintController

--- app/Http/Controllers/HelpDesk/ComplaintController.php
+++ app/Http/Controllers/HelpDesk/ComplaintController.php
@@ -208,14 +208,12 @@ class ComplaintController extends Controller
         } else {
             // Check if the tenant is a active resident of the building
             // Fetch the flat_tenant ID using the building_id, logged-in user's ID, and active status
-            $flatTenant = FlatTenant::where([
-                'flat_id'   => $request->flat_id,
-                'tenant_id' => auth()->user()->id,
-                'active'    => 1,
-            ])->first();
+            $flatTenant = FlatTenant::where('id',$request->flat_id)->first();
 
             $complaintableClass = FlatTenant::class;
             $complaitableId     = $flatTenant->id;
+
+
 
             if (!$flatTenant) {
                 return (new CustomResponseResource([
