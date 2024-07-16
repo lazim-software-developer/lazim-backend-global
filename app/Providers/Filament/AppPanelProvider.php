f275e75d9278f82227582a86cab6e8d0707b644a
Prashanth <prashanth.c@zysk.tech>
2024-07-17 01:31:38 +0530

    FIX: super admin color change

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -51,7 +51,7 @@ class AppPanelProvider extends PanelProvider
                 'danger' => Color::Rose,
                 'gray' => Color::Gray,
                 'info' => Color::Blue,
-                'primary' => Color::Indigo,
+                'primary' => Color::Orange,
                 'success' => Color::Emerald,
                 'warning' => Color::Orange,
                 'pink' => Color::Pink
