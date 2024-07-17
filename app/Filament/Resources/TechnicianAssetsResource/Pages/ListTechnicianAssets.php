ed49da60d82dc02eaf7d8fa5d10097265b0ff5dc
Prashanth <prashanth.c@zysk.tech>
2024-07-18 04:10:59 +0530

    FIX: vendor management

--- app/Filament/Resources/TechnicianAssetsResource/Pages/ListTechnicianAssets.php
+++ app/Filament/Resources/TechnicianAssetsResource/Pages/ListTechnicianAssets.php
@@ -19,7 +19,7 @@ class ListTechnicianAssets extends ListRecords
         if(Role::where('id', auth()->user()->role_id)->first()->name == 'Admin'){
             return parent::getTableQuery();
         }
-        return parent::getTableQuery()->whereIn('owner_association_id',Filament::getTenant()?->id);
+        return parent::getTableQuery()->where('owner_association_id',Filament::getTenant()?->id);
     }
 
     protected function getHeaderActions(): array
