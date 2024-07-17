70d54469d09bff6161efeb453b19799528de01c5
Subhshreddy <subhash.reddy@zysk.tech>
2024-07-17 20:29:42 +0530

    chore: Update DocumentObserver to log owner association slug and document URL

--- app/Observers/DocumentObserver.php
+++ app/Observers/DocumentObserver.php
@@ -35,7 +35,7 @@ class DocumentObserver
                         return $notifyTo->can($requiredPermissions);
                     });
                     Log::info(OwnerAssociation::where('id',$oam_id->owner_association_id)->first()?->slug);
-                    Log::info(OwnerAssociation::where('id',$oam_id->owner_association_id)->first()?->slug.TenantDocumentResource::getUrl('edit', $document->id));
+                    Log::info(OwnerAssociation::where('id',$oam_id->owner_association_id)->first()?->slug.TenantDocumentResource::getUrl('edit',[$document]));
                     Notification::make()
                         ->success()
                         ->title($document->name . " Received")
