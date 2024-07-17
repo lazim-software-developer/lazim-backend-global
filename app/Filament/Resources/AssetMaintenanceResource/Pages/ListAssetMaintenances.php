9db19ddd3b30f5642e9362f730064d4edb0d8464
Prashanth <prashanth.c@zysk.tech>
2024-07-18 04:07:29 +0530

    FIX: vendor management

--- app/Filament/Resources/AssetMaintenanceResource/Pages/ListAssetMaintenances.php
+++ app/Filament/Resources/AssetMaintenanceResource/Pages/ListAssetMaintenances.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\AssetMaintenanceResource\Pages;
 
 use App\Filament\Resources\AssetMaintenanceResource;
+use App\Models\Master\Role;
 use Filament\Actions;
+use Filament\Facades\Filament;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListAssetMaintenances extends ListRecords
 {
@@ -15,5 +18,12 @@ class ListAssetMaintenances extends ListRecords
         return [
             // Actions\CreateAction::make(),
         ];
+    }
+    protected function getTableQuery(): Builder
+    {
+        if(Role::where('id', auth()->user()->role_id)->first()->name == 'Admin'){
+            return parent::getTableQuery();
+        }
+        return parent::getTableQuery()->where('owner_association_id',Filament::getTenant()?->id);
     }
 }
