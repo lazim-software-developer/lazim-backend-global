f42bf49fb3bb6271abc9c61bc3cc1dc3a0ff8aaf
Subhshreddy <subhash.reddy@zysk.tech>
2024-07-17 19:24:29 +0530

    chore: Update authorization logic in PostController

--- app/Http/Controllers/Community/PostController.php
+++ app/Http/Controllers/Community/PostController.php
@@ -24,7 +24,7 @@ class PostController extends Controller
      */
     public function index(Request $request, Building $building)
     {
-        $this->authorize('viewAny');
+        $this->authorize('viewAny', [Post::class, $building->id]);
 
         // Start the query on the Post model
         $query = Post::where('status', 'published')
