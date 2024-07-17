6c7cd1a939b4d298b920808e78a490431878bde3
Prashanth <prashanth.c@zysk.tech>
2024-07-17 22:10:24 +0530

    FIX: custom pages building dropdown

--- app/Filament/Pages/GeneralFundStatementMollak.php
+++ app/Filament/Pages/GeneralFundStatementMollak.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Pages;
 
 use App\Models\Building\Building;
+use App\Models\Master\Role;
 use Carbon\Carbon;
+use Filament\Facades\Filament;
 use Filament\Pages\Page;
+use Illuminate\Support\Facades\DB;
 
 class GeneralFundStatementMollak extends Page
 {
@@ -19,9 +22,16 @@ class GeneralFundStatementMollak extends Page
     public function getViewData(): array
     {
         // $currentYear = Carbon::now()->year;
+        if(Role::where('id', auth()->user()->role_id)->first()->name == 'Admin'){
+            $buildings = Building::all();
+        }
+        else{
+            $buildings_id = DB::table('building_owner_association')->where('owner_association_id',Filament::getTenant()->id)->where('active', true)->pluck('building_id');
+            $buildings = Building::whereIn('id', $buildings_id)->get();
+        }
         return [
             // 'years' => range($currentYear, Carbon::now()->subYears(5)->year),
-            'buildings' => Building::where('owner_association_id', auth()->user()->owner_association_id)->get(),
+            'buildings' => $buildings,
             "message" => "Please Select a building and Year",
         ];
     }
--- app/Filament/Pages/TrialBalance.php
+++ app/Filament/Pages/TrialBalance.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Pages;
 
 use App\Models\Building\Building;
+use App\Models\Master\Role;
+use Filament\Facades\Filament;
 use Filament\Pages\Page;
+use Illuminate\Support\Facades\DB;
 
 class TrialBalance extends Page
 {
@@ -17,8 +20,15 @@ class TrialBalance extends Page
 
     public function getViewData(): array
     {
+        if(Role::where('id', auth()->user()->role_id)->first()->name == 'Admin'){
+            $buildings = Building::all();
+        }
+        else{
+            $buildings_id = DB::table('building_owner_association')->where('owner_association_id',Filament::getTenant()->id)->where('active', true)->pluck('building_id');
+            $buildings = Building::whereIn('id', $buildings_id)->get();
+        }
         return [
-            'buildings' => Building::where('owner_association_id', auth()->user()->owner_association_id)->get(),
+            'buildings' => $buildings,
             "message" => "Please Select a building and date",
         ];
     }
