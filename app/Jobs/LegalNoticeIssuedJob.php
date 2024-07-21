e5f31360b787dc4e2ee6fd0aae902c0669b5993b
Prashanth <prashanth.c@zysk.tech>
2024-07-22 00:02:29 +0530

    Webhook

--- app/Jobs/LegalNoticeIssuedJob.php
+++ app/Jobs/LegalNoticeIssuedJob.php
@@ -42,32 +42,34 @@ class LegalNoticeIssuedJob implements ShouldQueue
         ])->get("https://qagate.dubailand.gov.ae/mollak/external/sync/legalnotice/".$propertyGroupId."/".$mollakPropertyId."/".$legalNoticeId);
         // Log::info($results->json());
         
-        $responce = $results->json()['response']['propertyGroups'];
-        Log::info($responce);
-        $building_id = Building::where('property_group_id',$propertyGroupId)->first();
-        $oam_id = DB::table('building_owner_association')->where('building_id',$building_id?:null)->where('active', true)->first();
-        foreach($responce['mollakProperties'] as $notice){
-
-            $flat_id = Flat::where('mollak_property_id',$notice['mollakPropertyId'])->first();
-
-            $legalNotice = LegalNotice::updateOrCreate([
-                'building_id' => $building_id?->id,
-                'flat_id' => $flat_id?->id,
-                'invoicePeriod' => $notice['invoicePeriod'],
-                'owner_association_id' => $oam_id?->owner_association_id,
-            ],
-            [
-                'legalNoticeId' => $notice['legalNoticeId'],
-                'mollakPropertyId' => $notice['mollakPropertyId'],
-                'registrationDate' => $notice['registrationDate'],
-                'registrationNumber' => $notice['registrationNumber'],
-                'previousBalance' => $notice['previousBalance'],
-                'invoiceAmount' => $notice['invoiceAmount'],
-                'approvedLegalAmount' => $notice['approvedLegalAmount'],
-                'legalNoticePDF' => $notice['legalNoticePDF'],
-                'isRDCCaseStart' => $notice['isRDCCaseStart'],
-                'isRDCCaseEnd' => $notice['isRDCCaseEnd']
-            ]);
+        $responces = $results->json()['response']['propertyGroups'];
+        foreach($responces as $responce){
+            Log::info($responce);
+            $building_id = Building::where('property_group_id',$responce['propertyGroup']['id'])->first();
+            $oam_id = DB::table('building_owner_association')->where('building_id',$building_id?:null)->where('active', true)->first();
+            foreach($responce['mollakProperties'] as $notice){
+    
+                $flat_id = Flat::where('mollak_property_id',$notice['mollakPropertyId'])->first();
+    
+                $legalNotice = LegalNotice::updateOrCreate([
+                    'building_id' => $building_id?->id,
+                    'flat_id' => $flat_id?->id,
+                    'invoicePeriod' => $notice['invoicePeriod'],
+                    'owner_association_id' => $oam_id?->owner_association_id,
+                ],
+                [
+                    'legalNoticeId' => $notice['legalNoticeId'],
+                    'mollakPropertyId' => $notice['mollakPropertyId'],
+                    'registrationDate' => $notice['registrationDate'],
+                    'registrationNumber' => $notice['registrationNumber'],
+                    'previousBalance' => $notice['previousBalance'],
+                    'invoiceAmount' => $notice['invoiceAmount'],
+                    'approvedLegalAmount' => $notice['approvedLegalAmount'],
+                    'legalNoticePDF' => $notice['legalNoticePDF'],
+                    'isRDCCaseStart' => $notice['isRDCCaseStart'],
+                    'isRDCCaseEnd' => $notice['isRDCCaseEnd']
+                ]);
+            }
         }
         // } catch (\Exception $e) {
         //     Log::error('Failed to fetch legal notice issued ');
