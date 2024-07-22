f7c2a628743a2d2a7aee725915faac75295e0520
Prashanth <prashanth.c@zysk.tech>
2024-07-22 16:37:58 +0530

    FIX: incident listing

--- app/Filament/Resources/IncidentResource/Pages/ListIncidents.php
+++ app/Filament/Resources/IncidentResource/Pages/ListIncidents.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\IncidentResource\Pages;
 
 use App\Filament\Resources\IncidentResource;
+use App\Models\Master\Role;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListIncidents extends ListRecords
 {
@@ -15,5 +17,10 @@ class ListIncidents extends ListRecords
         return [
             // Actions\CreateAction::make(),
         ];
+    }
+
+    protected function getTableQuery(): Builder
+    {
+        return parent::getTableQuery()->where('type','incident');
     }
 }
