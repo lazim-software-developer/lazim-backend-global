eb0ac84ec35509815c946be07654117b013e4a00
Prashanth <prashanth.c@zysk.tech>
2024-07-23 17:20:49 +0530

    UPDATE: Create Tender Mail

--- app/Jobs/OAM/SendProposalRequestEmail.php
+++ app/Jobs/OAM/SendProposalRequestEmail.php
@@ -9,6 +9,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 use Snowfire\Beautymail\Beautymail;
@@ -20,7 +22,7 @@ class SendProposalRequestEmail implements ShouldQueue
     protected $vendors;
     protected $documentUrl;
 
-    public function __construct($vendors, $documentUrl,protected $emailCredentials)
+    public function __construct($vendors, $documentUrl,protected $mailCredentials)
     {
         $this->vendors = $vendors;
         $this->documentUrl = $documentUrl;
@@ -30,10 +32,17 @@ class SendProposalRequestEmail implements ShouldQueue
     {
         $beautymail = app()->make(\Snowfire\Beautymail\Beautymail::class);
 
+        Config::set('mail.mailers.smtp.host', $this->mailCredentials['mail_host']);
+        Config::set('mail.mailers.smtp.port', $this->mailCredentials['mail_port']);
+        Config::set('mail.mailers.smtp.username', $this->mailCredentials['mail_username']);
+        Config::set('mail.mailers.smtp.password', $this->mailCredentials['mail_password']);
+        Config::set('mail.mailers.smtp.encryption', $this->mailCredentials['mail_encryption']);
+        Config::set('mail.mailers.smtp.email', $this->mailCredentials['mail_from_address']);
+
         foreach ($this->vendors as $vendor) {
             $beautymail->send('emails.proposal_request', ['vendor' => $vendor], function ($message) use ($vendor) {
                 $message
-                    ->from($this->emailCredentials,env('MAIL_FROM_NAME'))
+                    ->from($this->mailCredentials['mail_from_address'],env('MAIL_FROM_NAME'))
                     ->to($vendor->user->email, $vendor->name)
                     ->subject('Request for Proposal');
 
@@ -45,6 +54,8 @@ class SendProposalRequestEmail implements ShouldQueue
                 ]);
             });
         }
+
+        Artisan::call('queue:restart');
     }
 
 }
