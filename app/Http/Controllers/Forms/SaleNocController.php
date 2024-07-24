11c387ec4226af76bfa4edb83d62872f84792030
Prashanth <prashanth.c@zysk.tech>
2024-07-24 12:29:47 +0530

    UPDATE: Sale Noc

--- app/Http/Controllers/Forms/SaleNocController.php
+++ app/Http/Controllers/Forms/SaleNocController.php
@@ -16,6 +16,7 @@ use App\Models\Order;
 use App\Models\OwnerAssociation;
 use Filament\Facades\Filament;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class SaleNocController extends Controller
@@ -25,16 +26,16 @@ class SaleNocController extends Controller
         // Upload files using the fucntion optimizeDocumentAndUpload
         $validated = $request->validated();
 
-        $ownerAssociationId = Building::find($request->building_id)->owner_association_id;
+        $oam_id = DB::table('building_owner_association')->where('building_id',$request->building_id)->where('active', true)->first();
 
         $validated['user_id']              = auth()->user()->id;
-        $validated['owner_association_id'] = $ownerAssociationId;
+        $validated['owner_association_id'] = $oam_id?->owner_association_id;
         $validated['submit_status']        = 'download_file';
         $validated['ticket_number']        = generate_ticket_number("SN");
 
         // Create the SaleNoc entry
         $saleNoc          = SaleNoc::create($validated);
-        $tenant           = Filament::getTenant()?->id ?? auth()->user()?->owner_association_id ?? $ownerAssociationId;
+        $tenant           = Filament::getTenant()?->id ?? $oam_id?->owner_association_id;
         // $emailCredentials = OwnerAssociation::find($tenant)?->accountcredentials()->where('active', true)->latest()->first()?->email ?? env('MAIL_FROM_ADDRESS');
 
         $credentials = AccountCredentials::where('oa_id', $tenant)->where('active', true)->latest()->first();
