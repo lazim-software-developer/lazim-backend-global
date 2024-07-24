fd4a0eab23645eb60e96a0c86f2cf7347691283e
Prashanth <prashanth.c@zysk.tech>
2024-07-24 18:57:03 +0530

    FIX: sale noc

--- app/Jobs/SendSaleNocEmail.php
+++ app/Jobs/SendSaleNocEmail.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 
@@ -19,7 +21,7 @@ class SendSaleNocEmail implements ShouldQueue
     protected $saleNoc;
     protected $documentPath;
 
-    public function __construct($saleNoc, $documentPath)
+    public function __construct($saleNoc, $documentPath, protected $mailCredentials)
     {
         $this->saleNoc = $saleNoc;
         $this->documentPath = $documentPath;
@@ -27,10 +29,18 @@ class SendSaleNocEmail implements ShouldQueue
 
     public function handle()
     {
+        Config::set('mail.mailers.smtp.host', $this->mailCredentials['mail_host']);
+        Config::set('mail.mailers.smtp.port', $this->mailCredentials['mail_port']);
+        Config::set('mail.mailers.smtp.username', $this->mailCredentials['mail_username']);
+        Config::set('mail.mailers.smtp.password', $this->mailCredentials['mail_password']);
+        Config::set('mail.mailers.smtp.encryption', $this->mailCredentials['mail_encryption']);
+        Config::set('mail.mailers.smtp.email', $this->mailCredentials['mail_from_address']);
+        
         $beautymail = app()->make(\Snowfire\Beautymail\Beautymail::class);
 
-        $beautymail->send('emails.salenoc_submitted', ['name' => $this->saleNoc->signing_authority_name], function ($message) {
+            $beautymail->send('emails.salenoc_submitted', ['name' => $this->saleNoc->signing_authority_name], function ($message) {
             $message
+                ->from($this->mailCredentials['mail_from_address'],env('MAIL_FROM_NAME'))
                 ->to($this->saleNoc->signing_authority_email)
                 ->subject('Sale NOC');
 
@@ -44,5 +54,7 @@ class SendSaleNocEmail implements ShouldQueue
                 'as' => 'sales_noc.'.$extension,
             ]);
         });
+
+        Artisan::call('queue:restart');
     }
 }
