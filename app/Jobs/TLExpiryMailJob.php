b9408af8743f0ea623eaf46ee2040b5d7820723d
Prashanth <prashanth.c@zysk.tech>
2024-07-25 12:15:04 +0530

    FIX: mail triggering

--- app/Jobs/TLExpiryMailJob.php
+++ app/Jobs/TLExpiryMailJob.php
@@ -8,6 +8,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Config;
 use Snowfire\Beautymail\Beautymail;
 
 class TLExpiryMailJob implements ShouldQueue
@@ -19,7 +21,7 @@ class TLExpiryMailJob implements ShouldQueue
      */
     public $user;
     public $vendor;
-    public function __construct($user, $vendor)
+    public function __construct($user, $vendor, protected $mailCredentials)
     {
         $this->user = $user;
         $this->vendor = $vendor;
@@ -30,11 +32,22 @@ class TLExpiryMailJob implements ShouldQueue
      */
     public function handle(): void
     {
+
+        Config::set('mail.mailers.smtp.host', $this->mailCredentials['mail_host']);
+        Config::set('mail.mailers.smtp.port', $this->mailCredentials['mail_port']);
+        Config::set('mail.mailers.smtp.username', $this->mailCredentials['mail_username']);
+        Config::set('mail.mailers.smtp.password', $this->mailCredentials['mail_password']);
+        Config::set('mail.mailers.smtp.encryption', $this->mailCredentials['mail_encryption']);
+        Config::set('mail.mailers.smtp.email', $this->mailCredentials['mail_from_address']);
+
         $beautymail = app()->make(Beautymail::class);
         $beautymail->send('emails.t_l_expiry_mail', ['user' => $this->user,'vendor' => $this->vendor], function($message) {
             $message
+                ->from($this->mailCredentials['mail_from_address'],env('MAIL_FROM_NAME'))
                 ->to($this->user->email, $this->user->first_name)
-                ->subject('Welcome to Lazim!');
+                ->subject('Reminder: Trade License Expiry');
         });
+
+        Artisan::call('queue:restart');
     }
 }
