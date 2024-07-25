13ef0d5446fabc21d238fee5490e63714b819ee1
Abhishek  M <abhishek.m@zysk.tech>
2024-07-25 17:15:31 +0530

    Added accoutnrelationmanager

--- app/Filament/Resources/OwnerAssociationResource/RelationManagers/AccountcredentialsRelationManager.php
+++ app/Filament/Resources/OwnerAssociationResource/RelationManagers/AccountcredentialsRelationManager.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\OwnerAssociationResource\RelationManagers;
 
+use App\Models\AccountCredentials;
 use Filament\Facades\Filament;
 use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\TextInput;
@@ -22,12 +23,60 @@ class AccountcredentialsRelationManager extends RelationManager
     {
         return $form
             ->schema([
+                TextInput::make('username')
+                    ->required()
+                    ->rules([
+                        'regex:/^[a-zA-Z0-9]+$/',
+                    ]),
                 TextInput::make('email')
                     ->required()
+                    ->minLength(6)
+                    ->maxLength(30)
                     ->rules(['min:6', 'max:30', 'regex:/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/']),
+                TextInput::make('password')
+                    ->required()
+                    ->minLength(8),
+                TextInput::make('mailer')
+                    ->required()
+                    ->string()
+                    ->minLength(3)
+                    ->maxLength(30),
+                TextInput::make('host')
+                    ->required()
+                    ->string()
+                    ->minLength(10)
+                    ->maxLength(35)
+                    ->rules([
+                        'regex:/^[a-z.]+$/'
+                    ]),
+                TextInput::make('port')
+                    ->required()
+                    ->integer()
+                    ->maxValue(9999),
+                TextInput::make('encryption')
+                    ->required()
+                    ->string()
+                    ->minLength(3)
+                    ->maxLength(30),
                 Toggle::make('active')
-                    ->inline(false)
-                    ->required(),
+                ->rules([
+                    'boolean',
+                    function ( $value, $fail) {
+                        if ($value) { 
+                            $existingActive = AccountCredentials::where('active', true)
+                                ->when(isset($this->record), function ($query) {
+                                    return $query->where('id', '!=', $this->record->id);
+                                })
+                                ->exists();
+            
+                            if ($existingActive) {
+                                $fail('Only one active account credential is allowed.');
+                            }
+                        }
+                    }
+                ])
+                ->reactive(),   
+                
                 Hidden::make('oa_id')
                     ->default(Filament::getTenant()?->id ?? auth()->user()?->owner_association_id),
                 Hidden::make('created_by')
@@ -40,7 +89,10 @@ class AccountcredentialsRelationManager extends RelationManager
         return $table
             ->defaultSort('created_at', 'desc')
             ->columns([
+                Tables\Columns\TextColumn::make('username'),
                 Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('host'),
+                Tables\Columns\TextColumn::make('mailer'),
                 Tables\Columns\IconColumn::make('active')
                     ->boolean()
                     ->trueIcon('heroicon-o-check-badge')
