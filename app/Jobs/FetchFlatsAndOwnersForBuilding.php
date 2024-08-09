3df1df9b00edddc4ca8e2dd564d4a42c3d39533e
Prashanth <prashanth.c@zysk.tech>
2024-08-09 16:30:38 +0530

    UPDATE: Customer Owner

--- app/Jobs/FetchFlatsAndOwnersForBuilding.php
+++ app/Jobs/FetchFlatsAndOwnersForBuilding.php
@@ -10,6 +10,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Http;
 use App\Models\Building\Flat;
 use App\Jobs\FetchOwnersForFlat;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class FetchFlatsAndOwnersForBuilding implements ShouldQueue
@@ -56,12 +57,28 @@ class FetchFlatsAndOwnersForBuilding implements ShouldQueue
                         ]
                     );
 
+                    // $connection = DB::connection('lazim_accounts');
+                    // $connection->table('flats')->insert([
+                    //         'property_number' => $property['unitNumber'],
+                    //         'mollak_property_id' => $property['mollakPropertyId'],
+                    //         'building_id' => $this->building->id,
+                    //         'owner_association_id' => $this->building->owner_association_id,
+                    //         'plot_number' => $property['plotNumber'],
+                    //         'suit_area' => $property['suitArea'],
+                    //         'actual_area' => $property['actualArea'],
+                    //         'balcony_area' => $property['balconyArea'],
+                    //         'applicable_area' => $property['applicableArea'],
+                    //         'virtual_account_number' => $property['virtualAccountNumber'],
+                    //         'parking_count' => $property['parkingCount'],
+                    //         'property_type' => 'NA'
+                    // ]);
+
                     // Dispatch job to fetch owners for the flat
                     FetchOwnersForFlat::dispatch($flat);
                 }
             }
         } catch (\Exception $e) {
-            Log::error("no responce from mollak");
+            Log::error("no responce from mollak". $this->building->property_group_id);
         }
     }
 }
