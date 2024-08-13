07328d0c3e9a862497b705991e6a9e063e914148
Subhshreddy <subhash.reddy@zysk.tech>
2024-08-13 17:45:24 +0530

    Merge branch 'dev' of https://github.com/zysk/lazim-backend into pre-prod

--- app/Filament/Resources/TenderResource/RelationManagers/ProposalsRelationManager.php
+++ app/Filament/Resources/TenderResource/RelationManagers/ProposalsRelationManager.php
@@ -177,7 +177,7 @@ class ProposalsRelationManager extends RelationManager
 
                             if (isset($contract, $vendor, $creator) && $exists==0) {
                                 $connection->table('venders')->insert([
-                                    'vender_id'       => $connection->table('venders')->latest()->first()?->id + 1,
+                                    'vender_id'       => $connection->table('venders')->where('created_by', $creator->id)->orderByDesc('vender_id')->first()?->vender_id + 1,
                                     'name'            => $vendor->name,
                                     'email'           => substr($creator->name, 0, 2) . $user->email,
                                     'password'        => '',
