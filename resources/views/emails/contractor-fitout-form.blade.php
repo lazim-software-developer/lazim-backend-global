0245b7d66f923bd04252dad3a4c875b5fa565793
Prashanth <prashanth.c@zysk.tech>
2024-08-26 12:46:43 +0530

    UPDATE: contractor fitout form

--- resources/views/emails/contractor-fitout-form.blade.php
+++ resources/views/emails/contractor-fitout-form.blade.php
@@ -21,7 +21,7 @@
 </tr>
 <tr>
 	<td class="paragraph">
-		<a href="https://lazim-resident.vercel.app/forms/fitout/{{$id}}">Link</a>
+		<a href= "{{ env('CONTRACTOR_REQUEST_PAGE') . '/' . $id }}">Link</a>
 	</td>
 </tr>
 <tr>
