87b1a01d9fe3e491a01e179e06a05869429c867e
Prashanth <prashanth.c@zysk.tech>
2024-08-26 20:32:59 +0530

    FIX: OA complaint

--- app/Filament/Resources/OacomplaintReportsResource/Pages/CreateOacomplaintReports.php
+++ app/Filament/Resources/OacomplaintReportsResource/Pages/CreateOacomplaintReports.php
@@ -31,36 +31,36 @@ class CreateOacomplaintReports extends CreateRecord
     protected function afterCreate()
     {
 
-        $expoPushToken = ExpoPushNotification::where('user_id', $this->record?->user_id)->first()?->token;
-        if ($expoPushToken) {
-            $message = [
-                'to'    => $expoPushToken,
-                'sound' => 'default',
-                'title' => 'Task Assigned',
-                'body'  => 'Task has been assigned',
-                'data'  => ['notificationType' => ''],
-            ];
-            $this->expoNotification($message);
-        }
-        DB::table('notifications')->insert([
-            'id'              => (string) \Ramsey\Uuid\Uuid::uuid4(),
-            'type'            => 'Filament\Notifications\DatabaseNotification',
-            'notifiable_type' => 'App\Models\User\User',
-            'notifiable_id'   => $this->record?->user_id,
-            'data'            => json_encode([
-                'actions'   => [],
-                'body'      => 'Task has been assigned',
-                'duration'  => 'persistent',
-                'icon'      => 'heroicon-o-document-text',
-                'iconColor' => 'warning',
-                'title'     => 'Task Assigned',
-                'view'      => 'notifications::notification',
-                'viewData'  => [],
-                'format'    => 'filament',
-                'url'       => '',
-            ]),
-            'created_at'      => now()->format('Y-m-d H:i:s'),
-            'updated_at'      => now()->format('Y-m-d H:i:s'),
-        ]);
-    }
+    //     $expoPushToken = ExpoPushNotification::where('user_id', $this->record?->user_id)->first()?->token;
+    //     if ($expoPushToken) {
+    //         $message = [
+    //             'to'    => $expoPushToken,
+    //             'sound' => 'default',
+    //             'title' => 'Task Assigned',
+    //             'body'  => 'Task has been assigned',
+    //             'data'  => ['notificationType' => ''],
+    //         ];
+    //         $this->expoNotification($message);
+    //     }
+    //     DB::table('notifications')->insert([
+    //         'id'              => (string) \Ramsey\Uuid\Uuid::uuid4(),
+    //         'type'            => 'Filament\Notifications\DatabaseNotification',
+    //         'notifiable_type' => 'App\Models\User\User',
+    //         'notifiable_id'   => $this->record?->user_id,
+    //         'data'            => json_encode([
+    //             'actions'   => [],
+    //             'body'      => 'Task has been assigned',
+    //             'duration'  => 'persistent',
+    //             'icon'      => 'heroicon-o-document-text',
+    //             'iconColor' => 'warning',
+    //             'title'     => 'Task Assigned',
+    //             'view'      => 'notifications::notification',
+    //             'viewData'  => [],
+    //             'format'    => 'filament',
+    //             'url'       => '',
+    //         ]),
+    //         'created_at'      => now()->format('Y-m-d H:i:s'),
+    //         'updated_at'      => now()->format('Y-m-d H:i:s'),
+    //     ]);
+    // }
 }
