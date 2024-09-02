6b03763e99e0721964de20e13d265378fae394e0
Abhishek M <abhishek.m@zysk.tech>
2024-09-02 19:33:35 +0530

    Fixed issue in user

--- app/Filament/Resources/User/UserResource.php
+++ app/Filament/Resources/User/UserResource.php
@@ -124,7 +124,7 @@ class UserResource extends Resource
             $roles = Role::whereNotIn('name', ['Admin', 'Technician', 'Security', 'Tenant', 'OA', 'Owner', 'Managing Director', 'Vendor'])->pluck('id');
         }
         return $table
-            ->modifyQueryUsing(fn(Builder $query) => $query->where('owner_association_id',auth()->user()?->owner_association_id)->whereIn('role_id',$roles))
+            ->modifyQueryUsing(fn(Builder $query) => $query->where('owner_association_id',auth()->user()?->owner_association_id)->whereIn('role_id',$roles)->where('id', '!=', auth()->user()->id))
             ->poll('60s')
             ->columns([
                 Tables\Columns\TextColumn::make('first_name')
