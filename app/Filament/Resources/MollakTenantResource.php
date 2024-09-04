565002d2a0f14cd1c468bd4273466f20385e5e56
Prashanth <prashanth.c@zysk.tech>
2024-09-04 13:59:42 +0530

    UPDATE: view and building filter for Tenant Resource

--- app/Filament/Resources/MollakTenantResource.php
+++ app/Filament/Resources/MollakTenantResource.php
@@ -4,12 +4,17 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\MollakTenantResource\Pages;
 use App\Filament\Resources\MollakTenantResource\RelationManagers;
+use App\Models\Building\Building;
+use App\Models\Master\Role;
 use App\Models\MollakTenant;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -26,7 +31,40 @@ class MollakTenantResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                    ->disabled(),
+                TextInput::make('contract_number')
+                    ->disabled(),
+                TextInput::make('emirates_id')
+                    ->formatStateUsing(
+                        function ($state) {
+                            return $state ?: 'NA';
+                        }
+                    )
+                    ->disabled(),
+                TextInput::make('license_number')->formatStateUsing(
+                    function ($state) {
+                        return $state ?: 'NA';
+                    }
+                )
+                    ->default('NA')
+                    ->disabled(),
+                TextInput::make('mobile')
+                    ->disabled(),
+                TextInput::make('email')
+                    ->disabled(),
+                TextInput::make('start_date')
+                    ->disabled(),
+                TextInput::make('end_date')
+                    ->disabled(),
+                TextInput::make('contract_status')
+                    ->disabled(),
+                Select::make('building_id')
+                    ->relationship('building', 'name')
+                    ->disabled(),
+                Select::make('flat_id')
+                    ->relationship('flat', 'property_number')
+                    ->disabled(),
             ]);
     }
 
@@ -36,6 +74,7 @@ class MollakTenantResource extends Resource
             ->columns([
                 TextColumn::make('name')
                     ->searchable()
+                    ->limit(25)
                     ->default('NA'),
                 TextColumn::make('contract_number')
                     ->searchable()
@@ -52,26 +91,40 @@ class MollakTenantResource extends Resource
                 TextColumn::make('email')
                     ->searchable()
                     ->default('NA'),
-                TextColumn::make('start_date')
-                    ->searchable()
-                    ->default('NA'),
-                TextColumn::make('end_date')
-                    ->searchable()
-                    ->default('NA'),
-                TextColumn::make('contract_status')
-                    ->searchable()
-                    ->default('NA'),
-                TextColumn::make('building.name')
-                    ->searchable()
-                    ->default('NA'),
-                TextColumn::make('flat.property_number')
-                    ->searchable()
-                    ->default('NA')
-                    ->label('Unit Number'),
+                // TextColumn::make('start_date')
+                //     ->searchable()
+                //     ->default('NA'),
+                // TextColumn::make('end_date')
+                //     ->searchable()
+                //     ->default('NA'),
+                // TextColumn::make('contract_status')
+                //     ->searchable()
+                //     ->default('NA'),
+                // TextColumn::make('building.name')
+                //     ->searchable()
+                //     ->default('NA'),
+                // TextColumn::make('flat.property_number')
+                //     ->searchable()
+                //     ->default('NA')
+                //     ->label('Unit Number'),
             ])
             ->defaultSort('created_at', 'desc')
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+            ])
             ->filters([
-                //
+                SelectFilter::make('building_id')
+                    ->options(function () {
+                        if (Role::where('id', auth()->user()->role_id)->first()->name == 'Admin') {
+                            return Building::all()->pluck('name', 'id');
+                        } else {
+                            return Building::where('owner_association_id', auth()->user()?->owner_association_id)
+                                ->pluck('name', 'id');
+                        }
+                    })
+                    ->searchable()
+                    ->preload()
+                    ->label('Building'),
             ]);
     }
 
@@ -86,6 +139,7 @@ class MollakTenantResource extends Resource
     {
         return [
             'index' => Pages\ListMollakTenants::route('/'),
+            'view' => Pages\ViewMollakTenant::route('/{record}'),
         ];
     }
 }
---
+++ app/Filament/Resources/MollakTenantResource/Pages/ViewMollakTenant.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\MollakTenantResource\Pages;
+
+use App\Filament\Resources\MollakTenantResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewMollakTenant extends ViewRecord
+{
+    protected static string $resource = MollakTenantResource::class;
+}
