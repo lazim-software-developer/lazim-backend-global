603dde95b84123323be1954e9303fa097b95b1c5
Prashanth <prashanth.c@zysk.tech>
2024-09-13 18:17:31 +0530

    UPDATE: List Vendor

--- app/Filament/Resources/Vendor/VendorResource/Pages/ListVendors.php
+++ app/Filament/Resources/Vendor/VendorResource/Pages/ListVendors.php
@@ -14,9 +14,9 @@ class ListVendors extends ListRecords
     protected static string $resource = VendorResource::class;
     protected function getTableQuery(): Builder
     {
-        if(Role::where('id', auth()->user()->role_id)->first()->name == 'Admin'){
+        // if(Role::where('id', auth()->user()->role_id)->first()->name == 'Admin'){
             return parent::getTableQuery();
-        }
+        // }
         // return parent::getTableQuery()->where('owner_association_id', Filament::getTenant()?->id);
     }
     protected function getHeaderActions(): array
