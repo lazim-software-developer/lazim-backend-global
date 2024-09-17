91450add47e151f1ec67bf23d1d6df664ce01058
Abhishek M <abhishek.m@zysk.tech>
2024-09-17 21:05:14 +0530

    Fixed issue in contract

--- app/Filament/Resources/ContractResource.php
+++ app/Filament/Resources/ContractResource.php
@@ -165,7 +165,8 @@ class ContractResource extends Resource
                                     $difference = round($difference, 2);
 
                                     $set('balance_amount', $difference);
-                                } else {
+                                } 
+                                if($contract == null) {
                                     $budgetAmount = $get('budget_amount');
                                     $set('balance_amount', $budgetAmount);
                                 }
