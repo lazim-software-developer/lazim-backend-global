b42fa67bd5fe5b159c00079ff66a6258e8df4f06
Subhshreddy <subhash.reddy@zysk.tech>
2024-09-19 23:33:12 +0530

    handelling the logout

---
+++ app/Filament/MyLogoutResponse.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament;
+
+use Filament\Facades\Filament;
+use Filament\Http\Responses\Auth\Contracts\LogoutResponse as LogoutResponseContract;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Log;
+
+class MyLogoutResponse
+{
+    public function toResponse($request): RedirectResponse
+    {
+        $panel = filament()->getId();
+        if ($panel === 'app') {
+            Filament::auth()->logout();
+            return redirect()->to(env('APP_URL') . '/app/login');
+        }
+    }
+}
