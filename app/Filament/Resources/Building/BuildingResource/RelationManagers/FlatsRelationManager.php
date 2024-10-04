e76f61c99a9361b888e34129a5bfbc9d345b6c8a
TazeenSoudagar <tazeen.soudagar@zysk.tech>
2024-10-04 17:03:28 +0530

    Update: FlatsRelationManager

--- app/Filament/Resources/Building/BuildingResource/RelationManagers/FlatsRelationManager.php
+++ app/Filament/Resources/Building/BuildingResource/RelationManagers/FlatsRelationManager.php
@@ -82,6 +82,7 @@ class FlatsRelationManager extends RelationManager
                 TextColumn::make('virtual_account_number')
                     ->default('NA')
                     ->searchable()
+                    ->hidden(in_array(auth()->user()->role->name, ['Property Manager', 'Admin']))
                     ->limit(50),
                 TextColumn::make('parking_count')
                     ->default('NA')
