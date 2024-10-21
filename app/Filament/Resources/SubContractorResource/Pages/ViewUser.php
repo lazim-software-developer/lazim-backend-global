4b598d6c0db8fc34b4a3d7141941496f3f6763ae
TazeenSoudagar <tazeen.soudagar@zysk.tech>
2024-10-21 16:47:19 +0530

    Fix: Issues Users created by the property manager cannot access the admin panel; 404 Not Found error on login. #1639, Inconsistencies on Edit Subcontractor Page #1645

---
+++ app/Filament/Resources/SubContractorResource/Pages/ViewUser.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\SubContractorResource\Pages;
+
+use App\Filament\Resources\SubContractorResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewUser extends ViewRecord
+{
+    protected static string $resource = SubContractorResource::class;
+}
