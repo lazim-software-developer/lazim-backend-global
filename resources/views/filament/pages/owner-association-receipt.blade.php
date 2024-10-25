e5d027303941390e7aa84598c12dd290d5e42043
Subhshreddy <subhash.reddy@zysk.tech>
2024-10-25 13:42:12 +0530

    redirecting to app is app else admin.

--- resources/views/filament/pages/owner-association-receipt.blade.php
+++ resources/views/filament/pages/owner-association-receipt.blade.php
@@ -1,6 +1,6 @@
 <x-filament-panels::page>
       @php
-        $redirectUrl = auth()->user()->role->name == 'Admin' ? '/app/owner-association-invoices' : '/admin/owner-association-invoices'
+        $redirectUrl =  filament()->getId() == 'app' ? '/app/owner-association-invoices' : '/admin/owner-association-invoices'
     @endphp
 <x-filament::breadcrumbs :breadcrumbs="[
     $redirectUrl => 'Back',
