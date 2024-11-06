b0be19a38f5f0b9d867e085db53f1c0d30375434
TazeenSoudagar <tazeen.soudagar@zysk.tech>
2024-11-06 16:49:26 +0530

    Feat: Bills Management

---
+++ app/Filament/Resources/BillResource/Pages/CreateBill.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\BillResource\Pages;
+
+use App\Filament\Resources\BillResource;
+use Auth;
+use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+
+class CreateBill extends CreateRecord
+{
+    protected static string $resource = BillResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $data['uploaded_by']       = Auth::id();
+        $data['status_updated_by'] = Auth::id();
+
+        return static::getModel()::create($data);
+    }
+
+}
---
+++ app/Filament/Resources/BillResource/Pages/EditBill.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Filament\Resources\BillResource\Pages;
+
+use App\Filament\Resources\BillResource;
+use App\Models\Building\Flat;
+use Filament\Resources\Pages\EditRecord;
+
+class EditBill extends EditRecord
+{
+    protected static string $resource = BillResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            // Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $bill       = $this->record;
+        $flatId     = $bill->flat['property_number'];
+        $buildingId = Flat::where('property_number', $flatId)->pluck('building_id');
+
+        return [
+            'building_id'       => $buildingId,
+            'type'              => $bill->type,
+            'amount'            => $bill->amount,
+            'month'             => $bill->month,
+            'due_date'          => $bill->due_date,
+            'uploaded_on'       => $bill->uploaded_on,
+            'status'            => $bill->status,
+            'uploaded_by'       => $bill->uploaded_by,
+            'status_updated_by' => $bill->status_updated_by,
+
+        ];
+    }
+
+    protected function getRedirectUrl(): string | null
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
