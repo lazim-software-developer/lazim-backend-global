4c0186c7aa181c53a695a000415db9d88db9cfdf
Subhshreddy <subhash.reddy@zysk.tech>
2024-11-19 12:28:03 +0530

    filters for bills and cheques

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -6,12 +6,19 @@ use App\Http\Resources\BillResource;
 use App\Models\Bill;
 use App\Models\Building\Flat;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class BillController extends Controller
 {
-    public function index(Flat $flat)
+    public function index(Flat $flat, Request $request)
     {
-        $bills = Bill::where('flat_id',$flat->id);
+        $bills = Bill::where('flat_id', $flat->id);
+
+        if ($request->filled('date')) {
+            $date = Carbon::createFromFormat('m-Y', $request->date);
+            $bills->whereMonth('month', $date->month)
+                  ->whereYear('month', $date->year);
+        }
 
         return BillResource::collection($bills->paginate(10));
     }
