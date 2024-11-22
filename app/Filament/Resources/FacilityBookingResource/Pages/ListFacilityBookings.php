63459de06e47cf15e876c5899a708f79c6395d28
Subhshreddy <subhash.reddy@zysk.tech>
2024-11-22 15:35:22 +0530

    fixed work permit listing issue.

--- app/Filament/Resources/FacilityBookingResource/Pages/ListFacilityBookings.php
+++ app/Filament/Resources/FacilityBookingResource/Pages/ListFacilityBookings.php
@@ -2,12 +2,13 @@
 
 namespace App\Filament\Resources\FacilityBookingResource\Pages;
 
-use App\Filament\Resources\FacilityBookingResource;
-use App\Models\Building\Building;
-use App\Models\Master\Role;
 use Filament\Actions;
+use App\Models\Master\Role;
+use App\Models\Building\Building;
+use Illuminate\Support\Facades\DB;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\FacilityBookingResource;
 
 class ListFacilityBookings extends ListRecords
 {
@@ -22,8 +23,9 @@ class ListFacilityBookings extends ListRecords
 
     protected function getTableQuery(): Builder
     {
-        $buildings = Building::all()->where('owner_association_id',
-            auth()->user()?->owner_association_id)->pluck('id')->toArray();
+        $buildings = DB::table('building_owner_association')
+            ->where('owner_association_id', auth()->user()->owner_association_id)
+            ->pluck('building_id')->toArray();
         if (Role::where('id', auth()->user()->role_id)->first()->name == 'Property Manager') {
             return parent::getTableQuery()
                 ->where('bookable_type', 'App\Models\WorkPermit')->whereIn('building_id', $buildings);
