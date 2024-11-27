80362d6d94cab52d162368b2e2a720ade23c9dce
Prashanth <prashanth.c@zysk.tech>
2024-11-27 13:33:50 +0530

    UPDATE: Accounting

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -47,6 +47,8 @@ class Dashboard extends BaseDashboard
     protected function getActions(): array
     {
         return [
+            Action::make('Accounting Module')
+                ->url(env('ACCOUNTING_URL')),
             Action::make('resetFilters')
                 ->label('Reset Filters')
                 ->color('danger')
