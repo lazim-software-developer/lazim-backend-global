40d92ae81fb12eeeda11bbfd63cc123c24cef0e3
TazeenSoudagar <tazeen.soudagar@zysk.tech>
2024-12-03 13:46:59 +0530

    Refactor Makani number handling in FlatTenant resource; streamline data retrieval and updates

--- app/Filament/Resources/Building/FlatTenantResource/Pages/EditFlatTenant.php
+++ app/Filament/Resources/Building/FlatTenantResource/Pages/EditFlatTenant.php
@@ -3,13 +3,13 @@
 namespace App\Filament\Resources\Building\FlatTenantResource\Pages;
 
 use App\Filament\Resources\Building\FlatTenantResource;
-use Filament\Actions;
+use DB;
 use Filament\Resources\Pages\EditRecord;
 
 class EditFlatTenant extends EditRecord
 {
     protected static string $resource = FlatTenantResource::class;
-    protected static ?string $title = 'Resident';
+    protected static ?string $title   = 'Resident';
 
     // protected function getHeaderActions(): array
     // {
@@ -17,4 +17,23 @@ class EditFlatTenant extends EditRecord
     //         Actions\DeleteAction::make(),
     //     ];
     // }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $makaniNumber = DB::table('documents')
+            ->where('name', 'Makani number')
+            ->where('flat_id', $data['flat_id'])
+            ->value('url');
+        $data['makani_number_url'] = $makaniNumber;
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        DB::table('documents')
+            ->where('name', 'Makani number')
+            ->where('flat_id', $data['flat_id'])
+            ->update(['url' => $data['makani_number_url']]);
+        return $data;
+    }
 }
