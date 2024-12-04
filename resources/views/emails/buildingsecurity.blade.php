ba04a6aa767e6172397b24c27c778407b30a6eae
Subhshreddy <subhash.reddy@zysk.tech>
2024-12-04 11:29:11 +0530

    Refactor: Update welcome email content for Building Security app

--- resources/views/emails/buildingsecurity.blade.php
+++ resources/views/emails/buildingsecurity.blade.php
@@ -13,7 +13,7 @@
 		</tr>
 		<tr>
 			<td class="paragraph">
-            We're excited to have you on board with Lazim! Your account has been successfully created, and we're thrilled to welcome you to our community.
+                Welcome to Lazim Gatekeeper App! We're here to make your security tasks simple and efficient.
 			</td>
 		</tr>
 		<tr>
@@ -21,52 +21,62 @@
 		</tr>
 		<tr>
 			<td class="title">
-            Your Account Details:
+                Your Login Details:
 			</td>
 		</tr>
-
 		<tr>
 			<td width="100%" height="10"></td>
 		</tr>
-
         <tr>
             <td class="paragraph">
-                <strong>Email: </strong> {{$user->email}}
+                <strong>Username: </strong> {{$user->email}}
 			</td>
 		</tr>
         <tr>
             <td class="paragraph">
-                <strong>Password: </strong> {{$password}}
-                <!-- (We recommend changing this password upon your first login for security reasons.) -->
-			</td>
-		</tr>
-        <tr>
-			<td width="100%" height="25"></td>
-		</tr>
-
-        <tr>
-            <td class="paragraph">
-            Thank you for choosing Lazim. We're confident that you'll find great value in our platform, and we look forward to serving you.
+                <strong>Temporary Password: </strong> {{$password}}
+                <br>(Change your password after logging in.)
 			</td>
 		</tr>
         <tr>
 			<td width="100%" height="25"></td>
 		</tr>
-
         <tr>
-			<td class="paragraph">
-               Warm regards,
-            </td>
+			<td class="title">
+                Quick Start:
+			</td>
+		</tr>
+        <tr>
+            <td class="paragraph">
+                • Download the App: <a href="{{ env('GATEKEEPER_LINK') }}">{{ env('GATEKEEPER_LINK') }}</a><br>
+                • Log In: Use the details above<br>
+                • Explore Features: Manage visitors, deliveries, and access points effortlessly
+			</td>
+		</tr>
+        <tr>
+			<td width="100%" height="25"></td>
+		</tr>
+        <tr>
+            <td class="paragraph">
+                Need help? Contact us anytime at info@sasfm.co or 043206789.
+			</td>
+		</tr>
+        <tr>
+			<td width="100%" height="25"></td>
+		</tr>
+        <tr>
+            <td class="paragraph">
+                We're excited to have you on board!
+			</td>
 		</tr>
 		<tr>
 			<td width="100%" height="5"></td>
 		</tr>
 		<tr>
 			<td class="paragraph">
-           Lazim team
+                - Lazim Team
 			</td>
 		</tr>
-
 		<tr>
 			<td width="100%" height="25"></td>
 		</tr>
