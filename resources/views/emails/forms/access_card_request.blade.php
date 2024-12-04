d8546998b2d1a821101d22b2d6f5e947c44b1b1d
Subhshreddy <subhash.reddy@zysk.tech>
2024-12-04 16:06:25 +0530

    Refactor: Update Access Card Request email template and include property manager name

--- resources/views/emails/forms/access_card_request.blade.php
+++ resources/views/emails/forms/access_card_request.blade.php
@@ -4,81 +4,85 @@
 
 @include('beautymail::templates.minty.contentStart')
 <tr>
-	<td class="paragraph">
-		Dear {{$user->first_name}},
-	</td>
+    <td class="paragraph">
+        Dear {{$user->first_name}},
+    </td>
 </tr>
 <tr>
-	<td width="100%" height="20"></td>
+    <td width="100%" height="20"></td>
 </tr>
 <tr>
-	<td class="paragraph">
-		We are pleased to inform you that your {{ $type }} request has been successfully submitted. Below are your ticket details:
-	</td>
+    <td class="paragraph">
+        We are pleased to confirm that your access card request has been successfully submitted.
+    </td>
 </tr>
 <tr>
-	<td width="100%" height="25"></td>
+    <td width="100%" height="25"></td>
 </tr>
 <tr>
-	<td class="title">
-		Ticket Details:
-	</td>
+    <td class="paragraph">
+        Below are the details of your ticket:
+    </td>
 </tr>
 <tr>
-	<td width="100%" height="10"></td>
+    <td width="100%" height="10"></td>
 </tr>
 <tr>
-	<td class="paragraph">
-		<strong>Ticket Number: </strong> {{$ticket_number}}
-	</td>
+    <td class="paragraph">
+        <strong>Ticket Number: </strong> {{$ticket_number}}
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		<strong>Building: </strong> {{$building}}
-	</td>
+    <td class="paragraph">
+        <strong>Building: </strong> {{$building}}
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		<strong>Flat: </strong> {{$flat}}
-	</td>
+    <td class="paragraph">
+        <strong>Flat: </strong> {{$flat}}
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		<strong>Type: </strong> {{$type}}
-	</td>
+    <td class="paragraph">
+        <strong>Request Type: </strong> Access Card
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		<strong>Card Type: </strong> {{$card_type}}
-	</td>
-</tr>
-
-<tr>
-	<td width="100%" height="25"></td>
+    <td class="paragraph">
+        <strong>Card Type: </strong> {{$card_type}}
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		Thank you for choosing Lazim. We are confident that you will find great value in our platform, and we look forward to serving you.
-	</td>
+    <td width="100%" height="25"></td>
 </tr>
 <tr>
-	<td width="100%" height="25"></td>
+    <td class="paragraph">
+        Thank you for choosing Lazim. We are committed to ensuring a seamless process and look forward to assisting you further.
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		Warm regards,
-	</td>
+    <td width="100%" height="25"></td>
 </tr>
 <tr>
-	<td width="100%" height="5"></td>
+    <td class="paragraph">
+        Regards,
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		The Lazim Team
-	</td>
+    <td width="100%" height="15"></td>
 </tr>
 <tr>
-	<td width="100%" height="25"></td>
+    <td>
+        <img src="{{url('images/logo.png')}}" alt="Lazim" style="max-width: 150px; height: auto;">
+    </td>
+</tr>
+<tr>
+    <td class="paragraph">
+        {{$property_manager_name}}
+    </td>
+</tr>
+<tr>
+    <td width="100%" height="25"></td>
 </tr>
 @include('beautymail::templates.minty.contentEnd')
 
