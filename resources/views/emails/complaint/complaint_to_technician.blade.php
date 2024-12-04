2f4d48d22c8659e3cf068825ae39473e21c3595d
Subhshreddy <subhash.reddy@zysk.tech>
2024-12-04 13:56:01 +0530

    Refactor: Enhance guest registration email content and include property manager name

--- resources/views/emails/complaint/complaint_to_technician.blade.php
+++ resources/views/emails/complaint/complaint_to_technician.blade.php
@@ -73,7 +73,7 @@
 </tr>
 <tr>
     <td>
-        <img src="{{url('images/logo.png')}}" alt="Company Logo" style="max-width: 150px; height: auto;">
+        <img src="{{url('images/logo.png')}}" alt="Lazim" style="max-width: 150px; height: auto;">
     </td>
 </tr>
 <tr>
