36446dd870b9467f9ec445b67155e713f6afd2ba
Subhshreddy <subhash.reddy@zysk.tech>
2024-12-04 16:22:47 +0530

    Refactor: Enhance Move In/Out email template and include property manager name

--- resources/views/emails/send_moveinout.blade.php
+++ resources/views/emails/send_moveinout.blade.php
@@ -4,85 +4,103 @@
 
 @include('beautymail::templates.minty.contentStart')
 <tr>
-	<td class="paragraph">
-		Dear {{$user->first_name}},
-	</td>
+    <td class="paragraph">
+        Dear {{$user->first_name}},
+    </td>
 </tr>
 <tr>
-	<td width="100%" height="20"></td>
+    <td width="100%" height="20"></td>
 </tr>
 <tr>
-	<td class="paragraph">
-		We are pleased to inform you that your {{ $type }} request has been successfully submitted. Below are your ticket details:
-	</td>
+    <td class="paragraph">
+        We are pleased to confirm that your {{ $type }} request has been successfully submitted.
+    </td>
 </tr>
 <tr>
-	<td width="100%" height="25"></td>
+    <td width="100%" height="25"></td>
 </tr>
 <tr>
-	<td class="title">
-		Ticket Details:
-	</td>
+    <td class="paragraph">
+        Below are the details of your request:
+    </td>
 </tr>
 <tr>
-	<td width="100%" height="10"></td>
+    <td class="title">
+        Ticket Details:
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		<strong>Ticket Number: </strong> {{$ticket_number}}
-	</td>
+    <td width="100%" height="10"></td>
 </tr>
 <tr>
-	<td class="paragraph">
-		<strong>Building: </strong> {{$building}}
-	</td>
+    <td class="paragraph">
+        ● <strong>Ticket Number: </strong> {{$ticket_number}}
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		<strong>Flat: </strong> {{$flat}}
-	</td>
+    <td class="paragraph">
+        ● <strong>Building: </strong> {{$building}}
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		<strong>Type: </strong> {{$type}}
-	</td>
+    <td class="paragraph">
+        ● <strong>Flat: </strong> {{$flat}}
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		<strong>Moving Date: </strong> {{$moving_date}}
-	</td>
+    <td class="paragraph">
+        ● <strong>Request Type: </strong> {{$type}}
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		<strong>Moving Time: </strong> {{$moving_time}}
-	</td>
+    <td class="paragraph">
+        ● <strong>Moving Date: </strong> {{$moving_date}}
+    </td>
 </tr>
 <tr>
-	<td width="100%" height="25"></td>
+    <td class="paragraph">
+        ● <strong>Moving Time: </strong> {{$moving_time}}
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		Thank you for choosing Lazim. We are confident that you will find great value in our platform, and we look forward to serving you.
-	</td>
+    <td width="100%" height="25"></td>
 </tr>
 <tr>
-	<td width="100%" height="25"></td>
+    <td class="paragraph">
+        Thank you for choosing us. We appreciate your cooperation and are here to ensure a smooth process for your {{ $type }}.
+    </td>
 </tr>
 <tr>
-	<td class="paragraph">
-		Warm regards,
-	</td>
+    <td class="paragraph">
+        If you need any further assistance, please feel free to reach out to us.
+    </td>
 </tr>
 <tr>
-	<td width="100%" height="5"></td>
+    <td width="100%" height="25"></td>
 </tr>
 <tr>
-	<td class="paragraph">
-		The Lazim Team
-	</td>
+    <td class="paragraph">
+        Regards,
+    </td>
 </tr>
 <tr>
-	<td width="100%" height="25"></td>
+    <td width="100%" height="15"></td>
+</tr>
+<tr>
+    <td>
+        <img src="{{url('images/logo.png')}}" alt="Company Logo" style="max-width: 150px; height: auto;">
+    </td>
+</tr>
+<tr>
+    <td width="100%" height="10"></td>
+</tr>
+<tr>
+    <td class="paragraph">
+        {{$property_manager_name}}
+    </td>
+</tr>
+<tr>
+    <td width="100%" height="25"></td>
 </tr>
 @include('beautymail::templates.minty.contentEnd')
 
