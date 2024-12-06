b155f3a6cec163582d46babee024d6671b71b44f
TazeenSoudagar <tazeen.soudagar@zysk.tech>
2024-12-06 17:37:02 +0530

    Refactor: Update patrolling query for Property Manager role and enable patrolling permissions

--- app/Filament/Resources/PatrollingResource/Pages/ListPatrollings.php
+++ app/Filament/Resources/PatrollingResource/Pages/ListPatrollings.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\PatrollingResource\Pages;
 
 use App\Filament\Resources\PatrollingResource;
 use App\Models\Master\Role;
+use DB;
 use Filament\Actions;
 use Filament\Facades\Filament;
 use Filament\Resources\Pages\ListRecords;
@@ -24,6 +25,16 @@ class ListPatrollings extends ListRecords
     {
         if(Role::where('id', auth()->user()->role_id)->first()->name == 'Admin'){
             return parent::getTableQuery();
+        }
+        if(Role::where('id', auth()->user()->role_id)->first()->name == 'Property Manager'){
+            $buildings = DB::table('building_owner_association')
+                ->where('owner_association_id', auth()->user()->owner_association_id)
+                ->where('active', true)
+                ->pluck('building_id');
+
+            return parent::getTableQuery()
+                ->whereIn('building_id', $buildings)
+                ->latest();
         }
         return parent::getTableQuery()->where('owner_association_id',Filament::getTenant()->id);
     }
