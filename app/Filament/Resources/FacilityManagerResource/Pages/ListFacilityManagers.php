8f76adf2b929c383389475ec7511446e38ba5c21
Subhshreddy <subhash.reddy@zysk.tech>
2024-12-24 12:05:06 +0530

    Refactor FacilityManager and Vendor resources: Update table queries to filter vendors based on owner association and user role

--- app/Filament/Resources/FacilityManagerResource/Pages/ListFacilityManagers.php
+++ app/Filament/Resources/FacilityManagerResource/Pages/ListFacilityManagers.php
@@ -3,16 +3,30 @@
 namespace App\Filament\Resources\FacilityManagerResource\Pages;
 
 use App\Filament\Resources\FacilityManagerResource;
+use App\Models\Role;
 use App\Models\User\User;
 use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
+use Filament\Facades\Filament;
 use Filament\Forms\Components\Select;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 
 class ListFacilityManagers extends ListRecords
 {
     protected static string $resource = FacilityManagerResource::class;
 
+    protected function getTableQuery(): Builder
+    {
+        $vendors = DB::table('owner_association_vendor')
+            ->where('owner_association_id', Filament::getTenant()?->id ?? auth()->user()->owner_association_id)
+            ->pluck('vendor_id');
+        if(Role::where('id', auth()->user()->role_id)->first()->name == 'Admin'){
+            return parent::getTableQuery();
+        }
+        return parent::getTableQuery()->whereIn('id', $vendors);
+    }
     protected function getHeaderActions(): array
     {
         return [
