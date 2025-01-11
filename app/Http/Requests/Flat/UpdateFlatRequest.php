d68cbc8f7897bbd3316fb27582aad1fdc8bc38bf
Dilip Shekhawat <laraveldev@lazim.ae>
2025-01-11 18:55:04 +0530

    Dilip-Flat-Module

---
+++ app/Http/Requests/Flat/UpdateFlatRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Flat;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateFlatRequest extends FormRequest
+{
+    public function rules()
+    {
+        $id = $this->segment(3);
+        return [
+            'floor' => 'required|string|max:255',
+            'building_id' => 'required|integer|exists:buildings,id',
+            'owner_association_id' => 'required|integer|exists:owner_associations,id',
+            'description' => 'required|string',
+            'property_number' => 'required|string',
+            'property_type' => 'required|string',
+            'suit_area' => 'required|string',
+            'actual_area' => 'required|string',
+            'balcony_area' => 'required|string',
+            'applicable_area' => 'required|string',
+            'virtual_account_number' => 'required|string',
+            'parking_count' => 'required|integer',
+            'plot_number' => 'required|integer',
+        ];
+    }
+}
