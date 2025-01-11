656ee8b4c96ecc77064fe1fb6a655064720734a7
Dilip Shekhawat <laraveldev@lazim.ae>
2025-01-11 17:44:23 +0530

    Dilip-Flat-Module

--- app/Http/Controllers/Building/FlatController.php
+++ app/Http/Controllers/Building/FlatController.php
@@ -1,15 +1,20 @@
 <?php
 
 namespace App\Http\Controllers\Building;
-
-use App\Http\Controllers\Controller;
-use App\Http\Resources\Building\FlatOwnerResource;
-use App\Http\Resources\Building\FlatResource;
-use App\Models\Building\Building;
+use Illuminate\Http\Request;
 use App\Models\Building\Flat;
+use App\Models\Building\Building;
+use App\Http\Controllers\Controller;
+use App\Repositories\FlatRepository;
+use App\Http\Resources\Building\FlatResource;
+use App\Http\Resources\Building\FlatOwnerResource;
 
 class FlatController extends Controller
 {
+    public function __construct(FlatRepository $repository)
+    {
+        $this->repository = $repository;
+    }
     public function fetchFlats(Building $building)
     {
         // $flats = $building->flats()->paginate(10);
@@ -22,6 +27,84 @@ class FlatController extends Controller
         // Check if flat exists
         if($flat) {
             return FlatOwnerResource::collection($flat->owners);
+        }
+    }
+
+    public function index(Request $request)
+    {
+        try {
+            $user = auth()->user();
+            $data = $this->repository->list($request);
+            return response()->json(["success"=>true,"message"=>'Data Found',"error"=>[],'data' => $data->paginate($request->per_page ?? 10)], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function store(StoreFlatRequest $request)
+    {
+        try {
+            $data = $this->repository->store($request->all());
+            return response()->json(['success' => true,'error' => [],'data' => new FlatResource($data), 'message' => 'Building created successfully'], 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function update(UpdateFlatRequest $request, $id)
+    {
+        try {
+            $data = $this->repository->update($id, $request->all());
+            return response()->json(['success' => true,'error' => [],'data' =>  new FlatResource($data), 'message' => 'Record updated successfully'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $this->repository->delete($id);
+            return response()->json(['success' => true,'error' => [],'data' =>  [],'message' => 'Record deleted successfully'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function changeStatus($id)
+    {
+        try {
+            $data = $this->repository->changeStatus($id);
+            return response()->json(['success' => true,'error' => [],'data' => ['id'=>$data->id,'status'=>$data->status], 'message' => 'Status updated successfully'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+    public function show($id)
+    {
+        try {
+            $building = $this->repository->show($id);
+            
+            // Using Resource to transform the data
+            return response()->json([
+                'success' => true,
+                'error' => [],
+                'data' => new FlatResource($building),
+                'message' => 'Flat details retrieved successfully'
+            ], 200);
+            
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Flat Detail not found'
+            ], 404);
+            
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Error retrieving Flat details',
+                'error' => $e->getMessage()
+            ], 500);
         }
     }
 }
---
+++ database/migrations/2025_01_11_161120_add_new_columns_in_flats_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasColumn('flats', 'resource')) {
+            Schema::table('flats', function (Blueprint $table) {
+                $table->unsignedBigInteger('created_by')->default(2);
+                $table->unsignedBigInteger('updated_by')->default(2);
+                $table->foreign('created_by')->references('id')->on('users');
+                $table->foreign('updated_by')->references('id')->on('users');
+                $table->string('resource')->default('Default');
+                $table->tinyInteger('status')->default(0)->comment('0: Inactive, 1: Active');
+                $table->softDeletes()->nullable();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('flats', function (Blueprint $table) {
+            $table->dropColumn('resource');
+            $table->dropColumn('status');
+            $table->dropColumn('created_by');
+            $table->dropColumn('updated_by');
+            $table->dropSoftDeletes();
+        });
+    }
+};
