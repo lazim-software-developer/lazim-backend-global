80bd0cede6fbd3742be108a10e40f7dd89fb84c9
Dilip Shekhawat <laraveldev@lazim.ae>
2025-01-11 17:45:48 +0530

    Dilip-Building-Module

--- database/migrations/2025_01_10_110921_add_new_columns_in_buildings_table.php
+++ database/migrations/2025_01_10_110921_add_new_columns_in_buildings_table.php
@@ -17,8 +17,7 @@ return new class extends Migration
                 $table->unsignedBigInteger('updated_by')->default(2);
                 $table->foreign('created_by')->references('id')->on('users');
                 $table->foreign('updated_by')->references('id')->on('users');
-                $table->enum('resource', ['Default', 'Mollak']);
-                $table->enum('status', ['0', '1']);
+                $table->string('resource')->default('Default');
                 $table->softDeletes()->nullable();
             });
         }
@@ -31,7 +30,6 @@ return new class extends Migration
     {
         Schema::table('buildings', function (Blueprint $table) {
             $table->dropColumn('resource');
-            $table->dropColumn('status');
             $table->dropColumn('created_by');
             $table->dropColumn('updated_by');
             $table->dropSoftDeletes();
