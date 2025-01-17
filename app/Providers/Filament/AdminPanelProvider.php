b67c5270d70e090e31117752943f35144796b6a4
TazeenSoudagar <tazeen.soudagar@zysk.tech>
2025-01-17 12:00:18 +0530

    Refactor role-based exclusions for OA in RoleResource and enhance FilamentShieldPlugin configuration

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -769,7 +769,22 @@ class AdminPanelProvider extends PanelProvider
                 Authenticate::class,
             ])
             ->plugins([
-                FilamentShieldPlugin::make(),
+                FilamentShieldPlugin::make()
+                    ->gridColumns([
+                        'default' => 1,
+                        'sm' => 2,
+                        'lg' => 3
+                    ])
+                    ->sectionColumnSpan(1)
+                    ->checkboxListColumns([
+                        'default' => 1,
+                        'sm' => 2,
+                        'lg' => 4,
+                    ])
+                    ->resourceCheckboxListColumns([
+                        'default' => 1,
+                        'sm' => 2,
+                    ]),
                 // FilamentApexChartsPlugin::make()
             ]);
     }
