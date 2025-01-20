c8f66249b4f34d30a6522e897231fcc8cd673feb
Subhshreddy <subhash.reddy@zysk.tech>
2025-01-20 18:38:31 +0530

    Add retrieval of owner association ID in create method of ComplaintController

--- app/Http/Controllers/HelpDesk/ComplaintController.php
+++ app/Http/Controllers/HelpDesk/ComplaintController.php
@@ -220,6 +220,8 @@ class ComplaintController extends Controller
      */
     public function create(ComplaintStoreRequest $request, Building $building)
     {
+        $oa_id = DB::table('building_owner_association')->where('building_id', $building->id)->where('active', true)->first()?->owner_association_id;
+
         if (auth()->user()->role->name == 'Security') {
             // Check if the gatekeeper has active member of the building
             $complaintableClass = User::class;
