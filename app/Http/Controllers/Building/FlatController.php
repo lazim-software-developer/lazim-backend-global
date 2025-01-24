705b32b0a0ee29fe3da56dbf26ae74aa063304db
Subhshreddy <subhash.reddy@zysk.tech>
2025-01-24 12:39:12 +0530

    Enhance FlatController to filter flats by property manager and owner association; update UserFlatResource to retrieve active owner associations

--- app/Http/Controllers/Building/FlatController.php
+++ app/Http/Controllers/Building/FlatController.php
@@ -8,13 +8,30 @@ use App\Http\Resources\Building\FlatResource;
 use App\Http\Resources\User\UserResource;
 use App\Models\Building\Building;
 use App\Models\Building\Flat;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FlatController extends Controller
 {
-    public function fetchFlats(Building $building)
+    public function fetchFlats(Building $building, Request $request)
     {
-        // $flats = $building->flats()->paginate(10);
         $flats = $building->flats()->with('ownerAssociation')->get();
+
+        if (!$request->has('type')) {
+            return FlatResource::collection($flats);
+        }
+
+        $pmFlats = DB::table('property_manager_flats')
+            ->whereIn('flat_id', $flats->pluck('id'))
+            ->where('active', true)
+            ->pluck('flat_id');
+
+        if ($request->type == 'Property Manager') {
+            $flats = Flat::whereIn('id', $pmFlats)->with('ownerAssociation')->get();
+        } elseif ($request->type == 'OA') {
+            $flats = $flats->whereNotIn('id', $pmFlats);
+        }
+
         return FlatResource::collection($flats);
     }
 
