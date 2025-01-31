b7296f1ce1f6e7bdfcdd85040b8a57c24eaa10a8
Subhshreddy <subhash.reddy@zysk.tech>
2025-01-31 14:20:27 +0530

    Fix: Refactor conditional logic in MoveInOutObserver for clarity

--- app/Observers/MoveInOutObserver.php
+++ app/Observers/MoveInOutObserver.php
@@ -36,8 +36,8 @@ class MoveInOutObserver
                 ->exists();
             $notifyTo = User::where('owner_association_id', $oa->id)->whereNotIn('role_id', $roles)
                 ->whereNot('id', auth()->user()?->id)->get();
-            if($oa->role == 'OA' && !$flatexists || ($oa->role == 'Property Manager' && $flatexists)){
-                if($moveInOut->type == 'move-in'){
+            if($moveInOut->type == 'move-in'){
+                if($oa->role == 'OA' && !$flatexists || ($oa->role == 'Property Manager' && $flatexists)){
                     $requiredPermissions = ['view_any_move::in::forms::document'];
                     $notifyTo->filter(function ($notifyTo) use ($requiredPermissions) {
                         return $notifyTo->can($requiredPermissions);
