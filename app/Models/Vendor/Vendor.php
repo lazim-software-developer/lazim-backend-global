bb7810a839f02af864dc27793f078a0e2fad8caf
Subhshreddy <subhash.reddy@zysk.tech>
2025-02-06 13:56:30 +0530

    Add owner association ID to pivot table in Vendor model

--- app/Models/Vendor/Vendor.php
+++ app/Models/Vendor/Vendor.php
@@ -100,7 +100,7 @@ class Vendor extends Model
     public function buildings()
     {
         return $this->belongsToMany(Building::class, 'building_vendor', 'vendor_id','building_id')
-                ->withPivot(['contract_id', 'active','start_date','end_date']);
+                ->withPivot(['contract_id', 'active','start_date','end_date','owner_association_id']);
     }
     public function buildingvendor()
     {
