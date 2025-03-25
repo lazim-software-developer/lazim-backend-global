aa95f19cf001699b30fd83bc02a8979911f304fa
Dilip Shekhawat <laraveldev@lazim.ae>
2025-03-25 09:48:31 +0530

    NBP-120

--- app/Filament/Resources/UserApprovalResource.php
+++ app/Filament/Resources/UserApprovalResource.php
@@ -104,10 +104,6 @@ class UserApprovalResource extends Resource
                         DatePicker::make('emirates_document_expiry_date')
                             ->label('Emirates ID Expiry')
                             ->disabled(!(Role::where('id', auth()->user()->role_id)->first()->name == 'Admin' || Role::where('id', auth()->user()->role_id)->first()->name == 'OA' || $user->can('update_user::approval'))),
-                        DatePicker::make('emirates_document_expiry_date')
-                            ->label('Emirates ID Expiry')
-                            ->disabled(!(Role::where('id', auth()->user()->role_id)->first()->name == 'Admin' || Role::where('id', auth()->user()->role_id)->first()->name == 'OA' || $user->can('update_user::approval')))
-                            ->minDate(Carbon::today()),
                         DatePicker::make('passport_expiry_date')
                             ->label('Passport Expiry')
                             ->minDate(Carbon::today())
