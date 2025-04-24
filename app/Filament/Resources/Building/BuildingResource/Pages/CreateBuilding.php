6bf3d7291eba7720e6dc8ad0a6b8339696dc2d0f
Subhshreddy <subhash.reddy@zysk.tech>
2025-04-24 15:04:39 +0530

    Fix: Refactor CreateBuilding methods for improved clarity and maintainability

--- app/Filament/Resources/Building/BuildingResource/Pages/CreateBuilding.php
+++ app/Filament/Resources/Building/BuildingResource/Pages/CreateBuilding.php
@@ -67,7 +67,7 @@ class CreateBuilding extends CreateRecord
 
     public function LazimAccountDatabase($data)
     {
-        $connection = DB::connection(config('database.connections.lazim_accounts'));
+        $connection = DB::connection('lazim_accounts');
         $created_by = $connection->table('users')->where('owner_association_id', $data->owner_association_id)->where('type', 'company')->first()?->id;
         $connection->table('users')->updateOrInsert([
             'building_id' => $data->id,
