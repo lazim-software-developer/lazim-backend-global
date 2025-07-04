e65a54c4c5e42bcddad2b111b1e3be47c4b78d61
Lazim Software Developer <140724566+lazim-software-developer@users.noreply.github.com>
2025-07-04 17:52:33 +0400

    update for the issue resolve of Notification View Page

--- app/Filament/Resources/NotificationListResource/Pages/ListNotificationSents.php
+++ app/Filament/Resources/NotificationListResource/Pages/ListNotificationSents.php
@@ -15,7 +15,7 @@ class ListNotificationSents extends ListRecords
     {
 
         return [
-            backButton(url: url()->previous())->visible(fn() => auth()->user()?->owner_association_id === 1), // TODO: Change this to the correct association ID or condition
+            backButton(url: url()->previous())->visible(), // TODO: Change this to the correct association ID or condition
             Actions\CreateAction::make(),
         ];
     }
